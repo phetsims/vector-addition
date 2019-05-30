190875b74486c06838dd3b00135b50b8bfcca287
Brandon Li <brandon.li820@icloud.com>
2019-05-29 20:57:31 -0600

    add component styles enum

---
+++ js/common/model/ComponentStyles.js
@@ -0,0 +1,26 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * Possible orientations for a vector
+ *
+ * @author Brandon Li
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const Enumeration = require( 'PHET_CORE/Enumeration' );
+  const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
+
+  /**
+   * The components can be displayed in 4 ways:
+   * 'INVISIBLE': don't show the components at all
+   * 'PARALLELOGRAM': the component's and the vector's initial points coincide
+   * 'TRIANGLE': the components are displayed head to tail
+   * 'ON_AXIS': the components are displayed on the x and y axis.
+   * @public
+   */
+  const ComponentStyles = new Enumeration( [ 'INVISIBLE', 'PARALLELOGRAM', 'TRIANGLE', 'ON_AXIS' ] );
+
+  return vectorAddition.register( 'ComponentStyles', ComponentStyles );
+} );
