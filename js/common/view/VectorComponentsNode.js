1fa5be988a20c2b258aad93a0fe61b404dbc6847
Martin Veillette <martin.veillette@gmail.com>
2019-06-03 09:10:07 -0600

    use Multilink to update vector components

--- js/common/view/VectorComponentsNode.js
+++ js/common/view/VectorComponentsNode.js
@@ -16,6 +16,7 @@ define( require => {
   const Node = require( 'SCENERY/nodes/Node' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
   const ComponentStyles = require( 'VECTOR_ADDITION/common/model/ComponentStyles' );
+  const Property = require( 'AXON/Property' );
 
   // constants
   const ARROW_OPTIONS = {
@@ -38,78 +39,71 @@ define( require => {
       super();
 
       // the origin of this node (0,0) is the tail of the vector.
-      // Create the 2 component nodes. These will be translated depending on the different component style
+      // create the 2 component nodes. These will be translated depending on the component style value
       const XComponentArrow = new ArrowNode( 0, 0, 0, 0, ARROW_OPTIONS );
       const YComponentArrow = new ArrowNode( 0, 0, 0, 0, ARROW_OPTIONS );
 
+      // add the components to the scene graph
       this.setChildren( [ XComponentArrow, YComponentArrow ] );
 
-      // create an object that matches enum value (key) to a function that changes the components to match 
-      // the keys style
-      const changeComponentsByComponentStyle = {
-        'INVISIBLE': () => {
-          this.visible = false;
-        },
-        'TRIANGLE': ( newVector ) => {
+      // create a function that updates the style of the components and their positions
+      const updateComponents = ( componentStyle, modelVector, modelTailPosition ) => {
 
-          // make the components visible
-          this.visible = true;
+        // calculate the vector (in view coordinates) based on its current value
+        const viewVector = modelViewTransform.modelToViewDelta( modelVector );
 
-          // calculate the tip position of the vector based on its current value
-          const tipPosition = modelViewTransform.modelToViewDelta( vector.vectorProperty.value );
+        // calculate the tail position of the vector
+        const viewTailPosition = modelViewTransform.modelToViewDelta( modelTailPosition );
 
-          XComponentArrow.setTailAndTip( 0, 0, tipPosition.x, 0 );
-          YComponentArrow.setTailAndTip( tipPosition.x, 0, tipPosition.x, tipPosition.y );
-        },
-        'PARALLELOGRAM': ( newVector ) => {
+        switch( componentStyle ) {
+          case ComponentStyles.INVISIBLE: {
+            this.visible = false;
+            break;
+          }
+          case ComponentStyles.TRIANGLE: {
+            // make the components visible
+            this.visible = true;
 
-          // make the components visible
-          this.visible = true;
+            XComponentArrow.setTailAndTip( 0, 0, viewVector.x, 0 );
+            YComponentArrow.setTailAndTip( viewVector.x, 0, viewVector.x, viewVector.y );
+            break;
+          }
+          case ComponentStyles.PARALLELOGRAM: {
+            // make the components visible
+            this.visible = true;
 
-          // calculate the tip position of the vector based on its current value
-          const tipPosition = modelViewTransform.modelToViewDelta( vector.vectorProperty.value );
-          XComponentArrow.setTailAndTip( 0, 0, tipPosition.x, 0 );
-          YComponentArrow.setTailAndTip( 0, 0, 0, tipPosition.y );
-        },
-        'ON_AXIS': ( newVector ) => {
-          // make the components visible
-          this.visible = true;
+            XComponentArrow.setTailAndTip( 0, 0, viewVector.x, 0 );
+            YComponentArrow.setTailAndTip( 0, 0, 0, viewVector.y );
+            break;
+          }
+          case  ComponentStyles.ON_AXIS: {
+            // make the components visible
+            this.visible = true;
 
-          // calculate the tip position of the vector based on its current value
-          const tipPosition = modelViewTransform.modelToViewDelta( vector.vectorProperty.value );
-
-          // calculate the tail position to place the components on the axis
-          const tailPosition = modelViewTransform.modelToViewDelta( vector.tailPositionProperty.value );
-
-          XComponentArrow.setTailAndTip( 0, -tailPosition.y, tipPosition.x, -tailPosition.y );
-          YComponentArrow.setTailAndTip( -tailPosition.x, 0, -tailPosition.x, tipPosition.y );
+            XComponentArrow.setTailAndTip( 0, -viewTailPosition.y, viewVector.x, -viewTailPosition.y );
+            YComponentArrow.setTailAndTip( -viewTailPosition.x, 0, -viewTailPosition.x, viewVector.y );
+            break;
+          }
+          default: {
+            throw new Error( 'invalid componentStyle: ' + componentStyle );
+          }
         }
       };
 
+      // @private
+      this.updateLayoutMultilink = Property.multilink( [
+          componentStyleProperty,
+          vector.vectorProperty,
+          vector.tailPositionProperty ],
+        ( componentStyle, vector, tailPosition ) => updateComponents( componentStyle, vector, tailPosition )
+      );
+    }
 
-      // create a link to the componentStyle enumeration property to toggle the different component styles
-      componentStyleProperty.link( ( newValue, oldValue ) => {
-
-        // first, if we have changed, remove the old component resize function
-        if ( oldValue ) {
-          vector.vectorProperty.unlink( changeComponentsByComponentStyle[ oldValue.name ] );
-
-          // on axis also was linked to the tail position property, so we have to remove it as well
-          if ( oldValue.name === ComponentStyles.ON_AXIS ) {
-            vector.tailPositionProperty.unlink( changeComponentsByComponentStyle[ oldValue.name ] );
-          }
-        }
-
-        // link the new component resize function based on the new value's name
-        vector.vectorProperty.link( changeComponentsByComponentStyle[ newValue.name ] );
-
-        // on axis style needs to be linked to the tail position
-        if ( newValue.name === ComponentStyles.ON_AXIS ) {
-          vector.tailPositionProperty.link( changeComponentsByComponentStyle[ newValue.name ] );
-        }
-      } );
-
-
+    /**
+     * @public
+     */
+    dispose() {
+      this.updateLayoutMultilink.dispose();
     }
   }
 
