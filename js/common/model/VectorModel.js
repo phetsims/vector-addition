a1bec42eca384d779d78c0111b040a4912c96a6a
Brandon Li <brandon.li820@icloud.com>
2019-06-28 21:24:28 -0600

    clean up vector model

--- js/common/model/VectorModel.js
+++ js/common/model/VectorModel.js
@@ -18,26 +18,28 @@ define( require => {
   const Animation = require( 'TWIXT/Animation' );
   const BaseVectorModel = require( 'VECTOR_ADDITION/common/model/BaseVectorModel' );
   const BooleanProperty = require( 'AXON/BooleanProperty' );
-  const ComponentStyles = require( 'VECTOR_ADDITION/common/model/ComponentStyles' );
   const DerivedProperty = require( 'AXON/DerivedProperty' );
   const Easing = require( 'TWIXT/Easing' );
   const GraphOrientations = require( 'VECTOR_ADDITION/common/model/GraphOrientations' );
   const Property = require( 'AXON/Property' );
   const Util = require( 'DOT/Util' );
   const Vector2 = require( 'DOT/Vector2' );
-  const Vector2Property = require( 'DOT/Vector2Property' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
-  const VectorGroups = require( 'VECTOR_ADDITION/common/model/VectorGroups' );
   const XVectorComponent = require( 'VECTOR_ADDITION/common/model/XVectorComponent' );
   const YVectorComponent = require( 'VECTOR_ADDITION/common/model/YVectorComponent' );
-
+  
   // constants
 
   // interval spacing of vector angle (in degrees) when vector is in polar mode
   const ANGLE_INTERVAL = 5;
 
-  const AVERAGE_ANIMATION_SPEED = 1000;
-  const MIN_ANIMATION_TIME = 1;
+  // The label of the vector when its active if and only if the user doesn't provide the label option.
+  // The reason this isn't translatable is: https://github.com/phetsims/vector-addition/issues/10.
+  const FALLBACK_VECTOR_LABEL = 'v';
+
+  const AVERAGE_ANIMATION_SPEED = 1000; // view coordinates per second
+  const MIN_ANIMATION_TIME = 10; // in seconds
+
 
   class VectorModel extends BaseVectorModel {
     /**
@@ -45,29 +47,27 @@ define( require => {
      * @param {Vector2} tailPosition
      * @param {number} xComponent horizontal component of the vector
      * @param {number} yComponent vertical component of the vector
-     * @param {Graph} graph
-     * @param {EnumerationProperty.<ComponentStyles>} componentStyleProperty
-     * @param {VectorGroups} vectorGroup - (see VectorGroups.js)
+     * @param {VectorSet} the vectorSet that the vector belongs to
      * @param {Object} [options]
      */
-    constructor( tailPosition, xComponent, yComponent, graph, componentStyleProperty, vectorGroup, options ) {
+    constructor( tailPosition, xComponent, yComponent, vectorSet, options ) {
 
       options = _.extend( {
-        label: 'v',// {string} - the label of the vector
-        isTipDraggable: true // {boolean} - can the tip be dragged
+        label: null, // {string|null} - the label of the vector. If null, the vector will display a the fallback label
+        // when its active
+
+        isTipDraggable: true // {boolean} - false means the tip won't be draggable
       }, options );
 
-      assert && assert( componentStyleProperty instanceof Property
-      && ComponentStyles.includes( componentStyleProperty.value ),
-        `invalid componentStyleProperty: ${componentStyleProperty}` );
-      assert && assert( VectorGroups.includes( vectorGroup ), `invalid vectorGroup: ${vectorGroup}` );
+
+      assert && assert( !options.label || typeof options.label === 'string',
+        `invalid options.label: ${options.label}` );
       assert && assert( typeof options.isTipDraggable === 'boolean',
-        `invalid isTipDraggable: ${options.isTipDraggable}` );
-      assert && assert( typeof options.label === 'string', `invalid options.label: ${options.label}` );
+        `invalid options.isTipDraggable: ${options.isTipDraggable}` );
 
       //----------------------------------------------------------------------------------------
 
-      super( tailPosition, xComponent, yComponent, vectorGroup );
+      super( tailPosition, xComponent, yComponent, vectorSet.vectorGroup );
 
       // @public (read-only) {boolean}
       this.isTipDraggable = options.isTipDraggable;
@@ -75,26 +75,23 @@ define( require => {
       // @public (read-only)
       this.label = options.label;
 
-      // TODO: remove this and add a activeVectorProperty in scene.js
-      // @public (read-only) {BooleanProperty} - indicates if the vector is active. An active vector is a vector that is
-      // being dragged by the body or the tip.
-      this.isActiveProperty = new BooleanProperty( false );
+      // @protected
+      this.fallbackLabel = FALLBACK_VECTOR_LABEL;
 
       // @private {Graph}
       this.graph = graph;
 
       //----------------------------------------------------------------------------------------
-      // Properties for the inspectPanel
+      // Properties for the 'Inspect a Vector' panel
 
       // @public (read-only) {DerivedProperty.<number>} - the magnitude of the vector
       this.magnitudeProperty = new DerivedProperty( [ this.attributesVectorProperty ], () => ( this.magnitude ) );
 
       // @public (read-only) {DerivedProperty.<number|null>} - the angle (in degrees) of the vector.
       // The angle is measured clockwise from the positive x-axis with angle in (-180,180]
-      this.angleDegreesProperty = new DerivedProperty( [ this.attributesVectorProperty ],
-        () => {
-          return ( this.attributesVector.equalsEpsilon( Vector2.ZERO, 1e-7 ) ) ? null : Util.toDegrees( this.angle );
-        } );
+      this.angleDegreesProperty = new DerivedProperty( [ this.attributesVectorProperty ], () => {
+        return ( this.attributesVector.equalsEpsilon( Vector2.ZERO, 1e-7 ) ) ? null : Util.toDegrees( this.angle );
+      } );
 
       // @public (read-only) {DerivedProperty.<number>} - the xComponent property
       this.xComponentProperty = new DerivedProperty( [ this.attributesVectorProperty ], () => ( this.xComponent ) );
@@ -104,16 +101,16 @@ define( require => {
 
       //----------------------------------------------------------------------------------------
 
-      // function to update the position of the tail of the vector based on the modelViewTransform
+      // Function to update the position of the tail of the vector based on the modelViewTransform
       const updateTailPosition = ( newModelViewTransform, oldModelViewTransform ) => {
         const oldTailViewPosition = oldModelViewTransform.modelToViewPosition( this.tailPositionProperty.value );
-        this.tailPositionProperty.value = newModelViewTransform.viewToModelPosition( oldTailViewPosition );
+        this.tail = newModelViewTransform.viewToModelPosition( oldTailViewPosition );
       };
 
       this.graph.modelViewTransformProperty.lazyLink( updateTailPosition );
 
       // @private - unlink the modelViewTransform link, called in the dispose method
-      this.unlinkTailUpdate = () => {
+      this.unlinkTailUpdateListener = () => {
         this.graph.modelViewTransformProperty.unlink( updateTailPosition );
       };
 
@@ -127,95 +124,113 @@ define( require => {
       this.yVectorComponent = new YVectorComponent( this, componentStyleProperty, this.label );
     
       //----------------------------------------------------------------------------------------
-      // Vector state
+      // Vector states
 
       // @public (read-only) {BooleanProperty} - indicates if the vector is in the play area
       this.isOnGraphProperty = new BooleanProperty( false );
 
-      // @public (read-only) {BooleanProperty} - tracks any animation that is currently in progress.
-      this.isAnimatingBackProperty = new BooleanProperty( false );
+      // @public (read-only) {Property.<Animation|null>} - tracks any animation that is currently in progress.
+      this.inProgressAnimationProperty = new Property( null, {
+        validValues: [ null, Animation ]
+      } );
 
     }
 
     /**
-     * Disposes the vector. Called when the vector is removed from the graph.
+     * Disposes the vector. Called when the vector is removed from the graph. This can be done by either
+     * animating the vector back to the vector creator panel or by hitting the eraser/reset all button.
      * @public
      * @override
      */
     dispose() {
-
-      this.isActiveProperty.dispose();
 
       this.magnitudeProperty.dispose();
       this.angleDegreesProperty.dispose();
       this.xComponentProperty.dispose();
       this.yComponentProperty.dispose();
 
-      this.unlinkTailUpdate();
+      this.unlinkTailUpdateListener();
 
       this.xVectorComponent.dispose();
       this.yVectorComponent.dispose();
 
       this.isOnGraphProperty.dispose();
-      this.isAnimatingBackProperty.dispose();
+
+      this.stopAnimation();
+      this.inProgressAnimationProperty.dispose();
 
       super.dispose();
     }
 
     /**
-     * Activates the vector for the inspect panel. Called when the vector is first dropped onto the graph.
+     * Called when the tip is dragged in the cartesian scene. Cartesian mode is defined as having integer x and y
+     * components.
+     * @param {Vector2} tipPosition - attempts to drag the tip to this position, but rounds it in cartesian style and
+     * updates the model tip
      * @public
      */
-    activate() {
-      this.isActiveProperty.value = true;
-    }
+    dragTipInCartesian( tipPosition ) {
 
-    /**
-     * Rounds vector to have integer values. Called when the tip is dragged in the cartesian scene
-     * @public
-     */
-    roundCartesianForm() {
+      assert && assert( !this.inProgressAnimationProperty.value, 'Cannot drag tip when vector is animating' );
+      assert && assert( this.isOnGraphProperty.value, 'Cannot drag tip when vector isn\'t on the graph' );
+      assert && assert( tipPosition instanceof Vector2, `invalid tipPosition: ${tipPosition}` );
 
-      const tipOnGraph = this.graph.graphModelBounds.closestPointTo( this.tip );
 
-      const cartesianTip = tipOnGraph.roundedSymmetric();
+      // Ensure that the tipPosition is on the graph
+      const tipPositionOnGraph = this.graph.graphModelBounds.closestPointTo( tipPosition );
 
-      const vectorInCartesianForm = cartesianTip.minus( this.tail );
+      // Rounded the tip to integer grid values
+      const roundedTipPosition = tipPositionOnGraph.roundedSymmetric();
+
+      // Get the components of the vector
+      const vectorComponents = roundedTipPosition.minus( this.tail );
 
       // Based on the vector orientation, constrain the dragging components
       if ( this.graph.orientation === GraphOrientations.HORIZONTAL ) {
-        vectorInCartesianForm.setY( 0 );
+        vectorComponents.setY( 0 );
       }
       else if ( this.graph.orientation === GraphOrientations.VERTICAL ) {
-        vectorInCartesianForm.setX( 0 );
+        vectorComponents.setX( 0 );
       }
-      this.attributesVector = vectorInCartesianForm;
+
+      // Update the model tip
+      this.tip = this.tail.plus( vectorComponents );
     }
 
     /**
-     * Rounds vector magnitude to an integer and angle to a multiple of ANGLE_INTERVAL. Called when the tip is dragged
-     * in the Polar scene
+     * Called when the tip is dragged in the polar scene. Polar mode is defined as having and integer magnitude and
+     * having the angle be a multiple of 5
+     * @param {Vector2} tipPosition - attempts to drag the tip to this position, but rounds it in polar style and
+     * updates the model tip
      * @public
      */
-    roundPolarForm() {
+    dragTipInPolar( tipPosition ) {
 
-      const roundedMagnitude = Util.roundSymmetric( this.magnitude );
-      const roundedAngle = ANGLE_INTERVAL * Util.roundSymmetric(
-        Util.toDegrees( this.angle ) / ANGLE_INTERVAL );
+      assert && assert( !this.inProgressAnimationProperty.value, 'Cannot drag tip when vector is animating' );
+      assert && assert( this.isOnGraphProperty.value, 'Cannot drag tip when vector isn\'t on the graph' );
+      assert && assert( tipPosition instanceof Vector2, `invalid tipPosition: ${tipPosition}` );
 
-      const polarVector = this.attributesVector.copy().setPolar( roundedMagnitude, Util.toRadians( roundedAngle ) );
+      const vectorAttributes = tipPosition.minus( this.tail );
+
+      const roundedMagnitude = Util.roundSymmetric( vectorAttributes.magnitude );
+      const roundedAngle = ANGLE_INTERVAL * Util.roundSymmetric( vectorAttributes.angle / ANGLE_INTERVAL );
+
+      // Calculate the rounded polar vector
+      const polarVector = vectorAttributes.setPolar( roundedMagnitude, roundedAngle );
 
       // Ensure that the new polar vector is in the bounds. Subtract one from the magnitude until the vector is inside.
       while ( !this.graph.graphModelBounds.containsPoint( this.tail.plus( polarVector ) ) ) {
         polarVector.setMagnitude( polarVector.magnitude - 1 );
       }
-      this.attributesVector = polarVector;
+
+      // Update the model tip
+      this.tip = this.tail.plus( polarVector );
     }
 
     /**
      * Updates the tail such that both tail and tip of the vector remain with the graphBounds. Called when the vector
      * body is being translated.
-     * @param {Vector2} tailPosition
+     * @param {Vector2} tailPosition - attempts to place the vector's tail to this point, but ensures it's validity
      * @public
      */
     moveVectorToFitInGraph( tailPosition ) {
@@ -233,30 +248,33 @@ define( require => {
       this.translateToPoint( constrainedBounds.closestPointTo( tailPosition ).roundedSymmetric() );
     }
 
-
     /**
      * Animates the vector to a specific point. Called when the user fails to drop the vector in the graph.
-     * @param {Vector2} position
+     * @param {Vector2} point
      * @param {Vector2} iconAttributesVector
-     * @param {function} callBack - callback when the animation is over
+     * @param {function} callBack - callback when the animation is over or stopped
      * @public
      */
-    animateToPoint( position, iconAttributesVector, callback ) {
+    animateToPoint( point, iconAttributesVector, callback ) {
 
 
-      assert && assert( position instanceof Vector2, `invalid position: ${position}` );
-      assert && assert( !this.isAnimatingBackProperty.value,
+      assert && assert( !this.inProgressAnimationProperty.value,
         'Can\'t animate to position when we are in animation currently' );
+      assert && assert ( !this.isOnGraphProperty.value, 'Can\'t animate when the vector is on the graph' );
 
-      this.isAnimatingBackProperty.value = true;
+      assert && assert( point instanceof Vector2, `invalid position: ${position}` );
+      assert && assert( iconAttributesVector instanceof Vector2,
+        `invalid iconAttributesVector: ${iconAttributesVector}` );
+      assert && assert( typeof callback === 'function', `invalid callback: ${callback}` );
+
+      //----------------------------------------------------------------------------------------
 
       // Convert the parameter into where the tail would be in that position
-      const tailPosition = position.setXY( position.x - iconAttributesVector.x, position.y );
+      const tailPosition = point.minus( iconAttributesVector );
 
       // Animate the vector
       const animation = new Animation( {
-        duration: Math.max(
-          MIN_ANIMATION_TIME,
+        duration: Math.max( MIN_ANIMATION_TIME,
           this.tail.distance( tailPosition ) / AVERAGE_ANIMATION_SPEED
         ),
         targets: [ {
@@ -268,16 +286,33 @@ define( require => {
           easing: Easing.CUBIC_IN_OUT,
           to: iconAttributesVector
         } ]
-      } );
+      } ).start();
 
-      animation.start();
 
-      // Remove the animation from the list when it finishes or is stopped
-      animation.finishEmitter.addListener( () => {
-        this.isAnimatingBackProperty.value = false;
+      const animationFinished = () => {
+        this.inProgressAnimationProperty.value = null;
         callback();
-      } );
 
+        // Remove listeners
+        animation.finishEmitter.removeListener( animationFinished );
+        animation.stopEmitter.removeListener( animationFinished );
+      }
+
+      // Reset properties and call the callback when the animation is either over
+      animation.finishEmitter.addListener( animationFinished );
+      animation.stopEmitter.addListener( animationFinished );
+
+    }
+
+    /**
+     * Stops the current animation if one is happening, and does nothing if one isn't happening
+     * @public
+     */
+    stopAnimation() {
+      if ( this.inProgressAnimationProperty.value ) {
+        this.inProgressAnimationProperty.value.stop();
+        this.inProgressAnimationProperty.value = null;
+      }
     }
 
     /**
