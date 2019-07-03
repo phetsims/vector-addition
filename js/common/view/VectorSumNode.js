0163b951bbd305a47452f30fa14bab1bde7677bf
Brandon Li <brandon.li820@icloud.com>
2019-07-03 11:41:05 -0600

    clean up vector sum node

--- js/common/view/VectorSumNode.js
+++ js/common/view/VectorSumNode.js
@@ -1,8 +1,12 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * Vector for the vector sum. Each vector set will have exactly one sum. Vector Sum Nodes have a distinct appearance for
- * each vector set.
+ * Vector for the vector sum.
+ *
+ * Extends Vector Node but adds the following functionality:
+ *  - a distinct appearance
+ *  - toggle visibility based on the sumVisibleProperty
+ *  - disables ability to take the sum vector node off of the graph
  *
  * @author Brandon Li
  */
@@ -12,40 +16,59 @@ define( require => {
 
   // modules
   const BooleanProperty = require( 'AXON/BooleanProperty' );
+  const Graph = require( 'VECTOR_ADDITION/common/model/Graph' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
   const VectorAdditionColors = require( 'VECTOR_ADDITION/common/VectorAdditionColors' );
   const VectorNode = require( 'VECTOR_ADDITION/common/view/VectorNode' );
+  const VectorSum = require( 'VECTOR_ADDITION/common/model/VectorSum' );
 
   class VectorSumNode extends VectorNode {
     /**
-     * @constructor
-     * @param {VectorModel} vectorModel- the vector model
+     * @param {VectorSum} vectorSum - the model for the vector sum
      * @param {Graph} graph - the graph the sum belongs to
      * @param {BooleanProperty} valuesVisibleProperty
      * @param {BooleanProperty} angleVisibleProperty
      * @param {BooleanProperty} sumVisibleProperty
      * @param {Object} [arrowOptions]
+     * @constructor
      */
-    constructor( vectorModel, graph, valuesVisibleProperty, angleVisibleProperty, sumVisibleProperty, arrowOptions ) {
+    constructor( vectorSum, graph, valuesVisibleProperty, angleVisibleProperty, sumVisibleProperty, arrowOptions ) {
 
+      assert && assert( vectorSum instanceof VectorSum, `invalid vectorSum: ${vectorSum}` );
+      assert && assert( graph instanceof Graph, `invalid graph: ${graph}` );
+      assert && assert( valuesVisibleProperty instanceof BooleanProperty,
+        `invalid valuesVisibleProperty: ${valuesVisibleProperty}` );
+      assert && assert( angleVisibleProperty instanceof BooleanProperty,
+        `invalid angleVisibleProperty: ${angleVisibleProperty}` );
       assert && assert( sumVisibleProperty instanceof BooleanProperty,
         `invalid sumVisibleProperty: ${sumVisibleProperty}` );
       assert && assert( !arrowOptions || Object.getPrototypeOf( arrowOptions ) === Object.prototype,
         `Extra prototype on arrowOptions: ${arrowOptions}` );
 
+      //----------------------------------------------------------------------------------------
+
       arrowOptions = _.extend( {
+
         // Passed to superclass
-        fill: VectorAdditionColors[ vectorModel.vectorGroup ].sum,
+        fill: VectorAdditionColors[ vectorSum.vectorGroup ].sum,
         lineWidth: 0.5
       }, arrowOptions );
 
       //----------------------------------------------------------------------------------------
 
-      super( vectorModel, graph, valuesVisibleProperty, angleVisibleProperty, arrowOptions );
+      super( vectorSum, graph, valuesVisibleProperty, angleVisibleProperty, arrowOptions );
 
       // Update the visibility of the sum node based on the sum visible property.
       // Doesn't need to be unlinked since vector sums are never disposed
       sumVisibleProperty.linkAttribute( this, 'visible' );
+
+      // Double check that the vector node never is animated back
+      // Doesn't need to be unlinked since vector sums are never disposed.
+      this.animateBackProperty.link( ( animateBack ) => {
+        if ( animateBack === true ) {
+          assert && assert( false, 'vector sums are never animated back' );
+        }
+      } );
     }
 
     /**
