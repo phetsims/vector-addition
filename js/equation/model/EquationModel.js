9ce984cd155b214d4bc2b04e97d37eed0da55f7b
Brandon Li <brandon.li820@icloud.com>
2019-07-10 10:58:49 -0600

    create scenes

--- js/equation/model/EquationModel.js
+++ js/equation/model/EquationModel.js
@@ -23,24 +23,11 @@ define( require => {
   // modules
   const BooleanProperty = require( 'AXON/BooleanProperty' );
   const CoordinateSnapModes = require( 'VECTOR_ADDITION/common/model/CoordinateSnapModes' );
-  const EnumerationProperty = require( 'AXON/EnumerationProperty' );
-  const EquationTypes = require( 'VECTOR_ADDITION/equation/model/EquationTypes' );
-  const EquationVectorSet = require( 'VECTOR_ADDITION/equation/model/EquationVectorSet' );
-  const Graph = require( 'VECTOR_ADDITION/common/model/Graph' );
-  const GraphOrientations = require( 'VECTOR_ADDITION/common/model/GraphOrientations' );
   const Tandem = require( 'TANDEM/Tandem' );
-  const Vector2 = require( 'DOT/Vector2' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
-  const VectorAdditionConstants = require( 'VECTOR_ADDITION/common/VectorAdditionConstants' );
   const VectorAdditionModel = require( 'VECTOR_ADDITION/common/model/VectorAdditionModel' );
   const VectorGroups = require( 'VECTOR_ADDITION/common/model/VectorGroups' );
-
-  // constants
-  const EQUATION_GRAPH_BOUNDS = VectorAdditionConstants.DEFAULT_GRAPH_BOUNDS
-    .withOffsets( 0, 0, 0, -VectorAdditionConstants.DEFAULT_VECTOR_LENGTH );
-  const STARTING_EQUATION_TYPE = EquationTypes.ADDITION;
-  const DEFAULT_BASE_VECTOR_VISIBILTY = false;
-  const DEFAULT_VECTOR_LENGTH = VectorAdditionConstants.DEFAULT_VECTOR_LENGTH;
+  const EquationScene = require( 'VECTOR_ADDITION/equation/model/EquationScene' );
 
   class EquationModel extends VectorAdditionModel {
     /**
@@ -66,54 +53,10 @@ define( require => {
       // Create the two graphs
 
       // @public (read-only) {graph}
-      this.polarGraph = new Graph( EQUATION_GRAPH_BOUNDS, CoordinateSnapModes.POLAR, GraphOrientations.TWO_DIMENSIONAL );
+      this.polarScene = new EquationScene( CoordinateSnapModes.POLAR, this.componentStyleProperty, VectorGroups.THREE );
 
       // @public (read-only) {graph}
-      this.cartesianGraph = new Graph( EQUATION_GRAPH_BOUNDS, CoordinateSnapModes.CARTESIAN, GraphOrientations.TWO_DIMENSIONAL );
-
-      //----------------------------------------------------------------------------------------
-      // Add properties (one for each scene) to control equation types (see ./EquationTypes.js)
-
-      // @public (read-only) {EnumerationProperty.<EquationTypes>}
-      this.polarEquationTypeProperty = new EnumerationProperty( EquationTypes, STARTING_EQUATION_TYPE );
-
-      // @public (read-only) {EnumerationProperty.<EquationTypes>}
-      this.cartesianEquationTypeProperty = new EnumerationProperty( EquationTypes, STARTING_EQUATION_TYPE );
-
-      //----------------------------------------------------------------------------------------
-      // Add properties (one for each scene) to control if the base vectors are visible
-
-      // @public (read-only) {BooleanProperty}
-      this.polarBaseVectorsVisibleProperty = new BooleanProperty( DEFAULT_BASE_VECTOR_VISIBILTY );
-
-      // @public (read-only) {BooleanProperty}
-      this.cartesianBaseVectorsVisibleProperty = new BooleanProperty( DEFAULT_BASE_VECTOR_VISIBILTY );
-
-      //----------------------------------------------------------------------------------------
-      // Create the vector sets for each graph
-
-      // @public (read-only) {EquationVectorSet}
-      this.polarVectorSet = new EquationVectorSet( this.polarGraph,
-        this.componentStyleProperty,
-        equationSumVisibleProperty,
-        VectorGroups.THREE,
-        CoordinateSnapModes.POLAR,
-        new Vector2( DEFAULT_VECTOR_LENGTH, DEFAULT_VECTOR_LENGTH ),
-        this.polarEquationTypeProperty
-      );
-
-      // @public (read-only) {EquationVectorSet}
-      this.cartesianVectorSet = new EquationVectorSet( this.cartesianGraph,
-        this.componentStyleProperty,
-        equationSumVisibleProperty,
-        VectorGroups.ONE,
-        CoordinateSnapModes.CARTESIAN,
-        new Vector2( DEFAULT_VECTOR_LENGTH, DEFAULT_VECTOR_LENGTH ),
-        this.cartesianEquationTypeProperty
-      );
-
-      this.polarGraph.vectorSets.push( this.polarVectorSet );
-      this.cartesianGraph.vectorSets.push( this.cartesianVectorSet );
+      this.cartesianScene = new EquationScene( CoordinateSnapModes.CARTESIAN, this.componentStyleProperty, VectorGroups.ONE );
 
     }
 
@@ -122,17 +65,9 @@ define( require => {
      */
     reset() {
 
-      // Reset base vectors visible properties
-      this.cartesianBaseVectorsVisibleProperty.reset();
-      this.polarBaseVectorsVisibleProperty.reset();
 
-      // Reset the properties to control equation types
-      this.polarEquationTypeProperty.reset();
-      this.cartesianEquationTypeProperty.reset();
-
-
-      this.polarGraph.reset();
-      this.cartesianGraph.reset();
+      // this.polarGraph.reset();
+      // this.cartesianGraph.reset();
       
       super.reset();
     }
