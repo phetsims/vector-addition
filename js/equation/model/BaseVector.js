499dbb327f03c30fc11b13873db62cf3c7bc85a2
Brandon Li <brandon.li820@icloud.com>
2019-07-17 11:54:14 -0600

    make base vector extend vector #63, and add properties for number picker

--- js/equation/model/BaseVector.js
+++ js/equation/model/BaseVector.js
@@ -4,7 +4,9 @@
  * See https://github.com/phetsims/vector-addition/issues/63 for context.
  *
  * Extends Vector and adds the following functionality:
- *  - Adjust its components based on componentNumberProperties that go into a number spinner
+ *  - Creates selector properties for the x and y component that go into a number picker on cartesian
+ *  - Creates selector properties for the angle and the magnitude that go into a number picker on polar
+ *  - Adjust its components based on the properties above
  *  - Disables tip dragging and removing of vectors
  *
  * Base vectors are created at the start of the sim, and are never disposed. They require a symbol.
@@ -16,80 +18,113 @@ define( require => {
   'use strict';
 
   // modules
+  const CoordinateSnapModes = require( 'VECTOR_ADDITION/common/model/CoordinateSnapModes' );
+  const NumberProperty = require( 'AXON/NumberProperty' );
   const Property = require( 'AXON/Property' );
-  const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
+  const Util = require( 'DOT/Util' );
   const Vector = require( 'VECTOR_ADDITION/common/model/Vector' );
-  const Vector2 = require( 'DOT/Vector2' );
+  const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
+  const Range = require( 'DOT/Range' );
+
+  // constants
+  const VECTOR_OPTIONS = {
+    isRemovable: false, // base vectors are not removable
+    isTipDraggable: false, // base vectors are not draggable by the tip
+    isOnGraphInitially: true // base vectors are always on the graph
+  };
+
+  // ranges
+  const COMPONENT_RANGE = new Range( -10, 10 );
+  const ANGLE_RANGE = new Range( -180, 180 );
+  const MAGNITUDE_RANGE = new Range( -10, 10 );
+
 
   class BaseVector extends Vector {
     /**
-     * @param {Vector2} tailPosition
-     * @param {number} xComponent horizontal component of the vector
-     * @param {number} yComponent vertical component of the vector
-     * @param {Graph} the graph the vector belongs to
-     * @param {EquationVectorSet} the equationVectorSet that the vector belongs to
+     * @param {Vector2} initialTailPosition - starting tail position of the vector
+     * @param {Vector2} initialComponents - starting components of the vector
+     * @param {EquationGraph} graph - the equation graph the vector belongs to
+     * @param {EquationVectorSet} vectorSet - the equationVectorSet that the vector belongs to
      * @param {string|null} symbol - the symbol for the vector (i.e. 'a', 'b', 'c', ...)
-     * @param {Object} [options]
      */
-    constructor( tailPosition, xComponent, yComponent, graph, equationVectorSet, symbol, options ) {
+    constructor( initialTailPosition, initialComponents, graph, vectorSet, symbol ) {
 
-      assert && assert( !options || Object.getPrototypeOf( options ) === Object.prototype,
-        `Extra prototype on Options: ${options}` );
-
-      // Disable tip dragging and removing of vectors
-      options = _.extend( {
-
-        // super class options
-        isRemovable: false,
-        isTipDraggable: false
-      }, options );
-
-
-      super( tailPosition, new Vector2( xComponent, yComponent ), graph, equationVectorSet, symbol, options );
+      super( initialTailPosition, initialComponents, graph, vectorSet, symbol, VECTOR_OPTIONS );
 
       //----------------------------------------------------------------------------------------
-      // Create Properties for the base vector panel
+      // Create properties for the base vector panel
 
-      // @public (read-only) {Property.<number>} - create a Property to represent the x component
-      this.xComponentSelectorProperty = new Property( this.xComponent, {
-        valueType: 'number'
-      } );
+      if ( graph.coordinateSnapMode === CoordinateSnapModes.CARTESIAN ) {
 
-      // @public (read-only) {Property.<number>} - create a Property to represent the y component
-      this.yComponentSelectorProperty = new Property( this.yComponent, {
-        valueType: 'number'
-      } );
+        // Creates selector properties for the x and y component that go into a number picker on cartesian
+
+        // @public (read-only) {NumberProperty} xComponentProperty - create a property to represent the component
+        this.xComponentProperty = new NumberProperty( this.xComponent );
+
+        // @public (read-only) {NumberProperty} yComponentProperty - create a property to represent the component
+        this.yComponentProperty = new NumberProperty( this.yComponent );
+
+        //----------------------------------------------------------------------------------------
+        // Create range properties
+        
+        // @public (read-only) {Property.<Range>} componentRangeProperty - property of the range of both components
+        this.componentRangeProperty = new Property( COMPONENT_RANGE );
+
+      }
+      else if ( graph.coordinateSnapMode === CoordinateSnapModes.POLAR ) {
+
+        // Creates selector properties for the angle and the magnitude that go into a number picker on polar
+
+        // @public (read-only) {NumberProperty} angleProperty - create a property to represent the angle of the vector
+        this.angleDegreesProperty = new NumberProperty( this.xComponent );
+
+        // @public (read-only) {NumberProperty} yComponentProperty - create a property to represent the magnitude
+        this.magnitudeProperty = new NumberProperty( this.yComponent );
+
+        //----------------------------------------------------------------------------------------
+        // Create range properties
+
+        // @public (read-only) {Property.<Range>} angleRangeProperty - property of the range of the angle (polar mode)
+        this.angleRangeProperty = new Property( ANGLE_RANGE );
+
+        // @public (read-only) {Property.<Range>} magnitudeRangeProperty - property of the range of the magnitude
+        this.magnitudeRangeProperty = new Property( MAGNITUDE_RANGE );
+      }
 
       //----------------------------------------------------------------------------------------
-      // Link the base vector panel Properties and update the components
-      // Both don't need to be unlinked since base vectors exist for the entire sim
+      // Link the base vector panel properties and update the components
+      // Don't need to be unlinked since base vectors exist for the entire sim
 
-      this.xComponentSelectorProperty.link( xComponent => {
+      this.xComponentProperty && this.xComponentProperty.link( xComponent => {
         this.xComponent = xComponent;
       } );
 
-      this.yComponentSelectorProperty.link( yComponent => {
+      this.yComponentProperty && this.yComponentProperty.link( yComponent => {
         this.yComponent = yComponent;
       } );
 
-      //----------------------------------------------------------------------------------------
-      // Double check that base vectors are never removed from the graph
+      this.angleDegreesProperty && this.angleDegreesProperty.link( angleDegrees => {
+        this.vectorComponents = this.vectorComponents.rotated( Util.toRadians( angleDegrees ) );
+      } );
 
-      this.isOnGraphProperty.value = true;
-
-      // Doesn't need to be unlinked; base vectors are never disposed
-      this.isOnGraphProperty.link( ( isOnGraph ) => {
-        if ( isOnGraph === false ) {
-          assert && assert( false, 'base vector models should never be off the graph' );
-        }
+      this.magnitudeProperty && this.magnitudeProperty.link( magnitude => {
+        this.vectorComponents = this.vectorComponents.withMagnitude( magnitude );
       } );
 
     }
 
+    /**
+     * Resets the base vector. Called when the reset all button is clicked.
+     * @public
+     */
     reset() {
-      this.yComponentSelectorProperty.reset();
-      this.xComponentSelectorProperty.reset();
       this.tailPositionProperty.reset();
+
+      this.angleDegreesProperty && this.angleDegreesProperty.reset();
+      this.magnitudeProperty && this.magnitudeProperty.reset();
+      this.yComponentProperty && this.yComponentProperty.reset();
+      this.xComponentProperty && this.xComponentProperty.reset();
+
     }
   }
 
