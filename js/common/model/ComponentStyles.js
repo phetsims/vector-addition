cd021025c1432374c365eceae86eec5cab4dfd2d
Brandon Li <brandon.li820@icloud.com>
2019-07-18 10:24:25 -0600

    #66 fix component terminology in js doc

--- js/common/model/ComponentStyles.js
+++ js/common/model/ComponentStyles.js
@@ -1,7 +1,7 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * Enumeration of the possible styles to display vector components.
+ * Enumeration of the possible 'styles' to display component vectors.
  *
  * @author Brandon Li
  */
@@ -14,8 +14,8 @@ define( require => {
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
 
   /**
-   * The components can be displayed in 4 ways:
-   * 'INVISIBLE': don't show the components at all
+   * The component vectors can be displayed in 4 ways:
+   * 'INVISIBLE': don't show the component vectors at all
    * 'PARALLELOGRAM': the component's and the vector's initial points coincide
    * 'TRIANGLE': the components are displayed head to tail
    * 'ON_AXIS': the components are displayed on the x and y axes.
