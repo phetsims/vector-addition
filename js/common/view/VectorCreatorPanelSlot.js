d93694fd86606aedb4d7aeb203ff32fa61f13989
Brandon Li <brandon.li820@icloud.com>
2019-07-26 12:43:45 -0600

    fix api of vector creator panel slot

--- js/common/view/VectorCreatorPanelSlot.js
+++ js/common/view/VectorCreatorPanelSlot.js
@@ -1,20 +1,23 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * View for a single slot that is on a vectorCreatorPanel.
+ * View for a single 'slot' on the VectorCreatorPanel (./VectorCreatorPanel.js).
  *
- * A slot creates a vector when the icon is clicked and allows the user to drag the screen onto graph.
+ * A slot creates a Vector when the icon is clicked.
  *
- * If the user doesn't drag the vector onto the graph, animate the vector
- * back to the panel slot: https://github.com/phetsims/vector-addition/issues/50.
+ * ## Slots can differ in:
+ *  - Icon colors and sizes
+ *  - Infinite slot versus only one vector per slot
+ *  - Having symbols versus not having symbols
+ *  - Icon components and initial vector components (e.g. on Explore 1D the initial vectors are horizontal/vertical
+ *    while on Explore 2D the vectors are 45 degrees)
  *
- * The vector creator panel differs from screen to screen in the following ways.
- *  - Different icon colors and sizes
- *  - Infinite slots versus only one vector per slot
- *  - Number of slots per panel
- *  - Having labels vs not having labels (called symbols)
- *  - Icon and initial vector components (i.e. on explore 1D the initial vector are horizontal/vertical) while
- *    on explore 2d the vectors are 45 degrees)
+ * ## Implementation of creation of Vectors:
+ *  1. Once the icon is clicked, a Vector is made.
+ *  2. A call to the Scene Node is made, passing the created Vector. The Scene Node then creates the subsequent views
+ *     for the Vector (Vector Node and Vector Component Node), layering the views correctly and forwarding the event.
+ *  3. Once the Vector indicates the Vector was dropped outside the Graph, the slot will then animate the Vector and
+ *     dispose the vector, signaling to the Scene Node to dispose of the views.
  *
  * @author Brandon Li
  */
@@ -23,167 +26,161 @@ define( require => {
   'use strict';
 
   // modules
-  // const VectorAdditionModel = require( 'VECTOR_ADDITION/common/model/VectorAdditionModel' );
   const AlignBox = require( 'SCENERY/nodes/AlignBox' );
   const Bounds2 = require( 'DOT/Bounds2' );
   const DragListener = require( 'SCENERY/listeners/DragListener' );
   const FormulaNode = require( 'SCENERY_PHET/FormulaNode' );
   const Graph = require( 'VECTOR_ADDITION/common/model/Graph' );
   const HBox = require( 'SCENERY/nodes/HBox' );
-  const Node = require( 'SCENERY/nodes/Node' );
-  const ScreenView = require( 'JOIST/ScreenView' );
   const Vector = require( 'VECTOR_ADDITION/common/model/Vector' );
   const Vector2 = require( 'DOT/Vector2' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
   const VectorAdditionIconFactory = require( 'VECTOR_ADDITION/common/view/VectorAdditionIconFactory' );
-  const VectorNode = require( 'VECTOR_ADDITION/common/view/VectorNode' );
   const VectorSet = require( 'VECTOR_ADDITION/common/model/VectorSet' );
 
   // constants
-  const LABEL_AND_ICON_SPACING = 6; // spacing between the label and the icon
-  const LABEL_RESIZE_SCALE = 0.8; // the label is resized to 0.8
-  const ICON_OFFSET_MOUSE_AREA = 8; // icon offset to make it easier to grab
+  const ICON_OFFSET_MOUSE_AREA = 8; // icon offset to make the icon easier to grab
+
+  // The fixed-width of the parent of the icon. The Icon is placed in an alignBox to ensure the Icon
+  // contains the same local width regardless of the initial vector components. This ensures that
+  // the label of the slot is in the same place regardless of the icon size.
+  const ARROW_ICON_CONTAINER_WIDTH = 35;
+
 
   class VectorCreatorPanelSlot extends HBox {
+
     /**
-     * @param {VectorAdditionModel} vectorAdditionModel
-     * @param {Vector2} initialVector - the initial vector's components, in model coordinates.
      * @param {Graph} graph - the graph to drop the vector onto
-     * @param {VectorSet} vectorSet - the vectorSet that the slot adds vectors to.
-     * @param {Node} vectorContainer - the container to add new vector nodes to (to keep vectors in a separate z-layer)
-     * @param {ScreenView} parentScreenView - the screen view up the scene graph for the creator panel slot
+     * @param {VectorSet} vectorSet - the vectorSet that the slot adds vectors to
+     * @param {SceneNode} sceneNode - the scene node the slot belongs in
+     * @param {Vector2} initialVectorComponents - the initial vector components to pass to created vectors
      * @param {Object} [options]
      */
-    constructor( vectorAdditionModel, initialVector, graph, vectorSet, vectorContainer, parentScreenView, options ) {
+    constructor( graph, vectorSet, sceneNode, initialVectorComponents, options ) {
 
-      // assert && assert( vectorAdditionModel instanceof VectorAdditionModel,
-      //   `invalid vectorAdditionModel: ${vectorAdditionModel}` );
-      assert && assert( initialVector instanceof Vector2, `invalid initialVector: ${initialVector}` );
       assert && assert( graph instanceof Graph, `invalid graph: ${graph}` );
       assert && assert( vectorSet instanceof VectorSet, `invalid vectorSet: ${vectorSet}` );
-      assert && assert( vectorContainer instanceof Node, `invalid vectorContainer: ${vectorContainer}` );
-      assert && assert( parentScreenView instanceof ScreenView, `invalid parentScreenView: ${parentScreenView}` );
+      assert && assert( initialVectorComponents instanceof Vector2,
+        `invalid initialVectorComponents: ${initialVectorComponents}` );
       assert && assert( !options || Object.getPrototypeOf( options ) === Object.prototype,
         `Extra prototype on Options: ${options}` );
 
       //----------------------------------------------------------------------------------------
 
       options = _.extend( {
-        symbol: null, // {string|null} the symbol for the vector at the slot
-        isInfinite: false, // {boolean} true means the slot will regenerate vectors to be dragged
-        labelIconSpacing: LABEL_AND_ICON_SPACING, // {number} spacing between the label and the icon
-        iconOptions: null, // {object} options passed to the icon. Defaults bellow.
-        arrowIconContainerWidth: 35 // {number} the fixed size of the container containing the icon
+
+        symbol: null,       // {string|null} the symbol to pass to created vectors
+        isInfinite: false,  // {boolean} indicates if the slot regenerates vectors once clicked
+        iconArrowSize: 30   // {number} indicates the magnitude of the icon in view coordinates
+
       }, options );
 
-      options.iconOptions = _.extend( {
-        arrowSize: 30
-      }, options.iconOptions );
+      super( { spacing: 5 } );
 
-      super( {
-        spacing: options.labelIconSpacing
-      } );
+      // convenience reference
+      const modelViewTransform = graph.modelViewTransformProperty.value;
 
       //----------------------------------------------------------------------------------------
+      // Create the icon
+      //----------------------------------------------------------------------------------------
 
-      // Convenience variables
-      const modelViewTransformProperty = graph.modelViewTransformProperty;
-      const initialViewVector = modelViewTransformProperty.value.modelToViewDelta( initialVector );
+      // Get the components in view coordinates.
+      const iconViewComponents = modelViewTransform.viewToModelDelta( initialVectorComponents );
 
-      // Create an icon
-      const iconNode = VectorAdditionIconFactory.createVectorCreatorPanelIcon( initialViewVector,
-        vectorSet.vectorColorGroup,
-        options.iconOptions );
-
-      // Get the components in model coordinates of the icon
-      const iconComponents = modelViewTransformProperty.value.viewToModelDelta(
-        initialViewVector.normalized().timesScalar( options.iconOptions.arrowSize ) );
+      // Create the icon.
+      const iconNode = VectorAdditionIconFactory.createVectorCreatorPanelIcon( iconViewComponents,
+        vectorSet.vectorColorGroup, {
+          arrowLength: options.iconArrowSize
+        } );
 
       // Make the iconNode easier to grab
       iconNode.mouseArea = iconNode.shape.getOffsetShape( ICON_OFFSET_MOUSE_AREA );
 
-      // Create a fixed size box for the icon, allowing the label to stay in the same spot
+      // Get the components in model coordinates of the icon. Used to animate the vector to the icon components.
+      const iconComponents = modelViewTransform.viewToModelDelta( iconViewComponents
+                                                                  .normalized().timesScalar( options.iconArrowSize ) );
+
+
+      // Create a fixed-size box for the icon. The Icon is placed in an alignBox to ensure the Icon
+      // contains the same local width regardless of the initial vector components. This ensures that
+      // the label of the slot is in the same place regardless of the icon size.
       this.addChild( new AlignBox( iconNode, {
-        alignBounds: new Bounds2( 0, 0, options.arrowIconContainerWidth, iconNode.height ),
-        xAlign: 'center',
-        yAlign: 'center'
+        alignBounds: new Bounds2( 0, 0, ARROW_ICON_CONTAINER_WIDTH, iconNode.height )
       } ) );
 
-      // Add the label if a symbol was provided
-      if ( options.symbol ) {
-        const label = new FormulaNode( `\\vec{\\mathrm{${options.symbol}}}` );
-        label.scale( LABEL_RESIZE_SCALE );
+      //----------------------------------------------------------------------------------------
+      // Create the label of the slot
+      //----------------------------------------------------------------------------------------
 
-        this.addChild( label );
+      if ( options.symbol ) {
+        this.addChild( new FormulaNode( `\\vec{${options.symbol}\}`, { scale: 0.95 } ) );
       }
 
       //----------------------------------------------------------------------------------------
-      // When the vector icon is clicked, create a new vector model
+      // Creation of Vectors (See ## Implementation of creation of Vectors above)
       //----------------------------------------------------------------------------------------
       iconNode.addInputListener( DragListener.createForwardingListener( ( event ) => {
 
-        // Get the model coordinates of where the icon was clicked
-        const globalPoint = parentScreenView.globalToLocalPoint( event.pointer.point );
-        const vectorCenterPosition = modelViewTransformProperty.value.viewToModelPosition( globalPoint );
+        //----------------------------------------------------------------------------------------
+        // Step 1: When the icon is clicked, create a new Vector
+        //----------------------------------------------------------------------------------------
 
-        // From the center, we can calculate where the tail would be based on the initialVector
-        const vectorTailPosition = vectorCenterPosition.minus( initialVector.timesScalar( 0.5 ) );
+        // Find where the icon was clicked relative to the scene node (view coordinates)
+        const vectorCenterLocation = sceneNode.globalToLocalPoint( event.pointer.point );
+
+        // Convert the view coordinates of where the icon was clicked into model coordinates
+        const vectorCenterPosition = graph.modelViewTransformProperty.value.viewToModelPosition( vectorCenterLocation );
+
+        // Calculate where the tail position is relative to the scene node
+        const vectorTailPosition = vectorCenterPosition.minus( initialVectorComponents.timesScalar( 0.5 ) );
 
         // Create the new Vector Model
-        const vector = new Vector( vectorTailPosition, initialVector, graph, vectorSet, options.symbol );
+        const vector = new Vector( vectorTailPosition, initialVectorComponents, graph, vectorSet, options.symbol );
+
         vectorSet.vectors.push( vector );
 
         //----------------------------------------------------------------------------------------
-        // Create the vector node and add it to the container
-        const vectorNode = new VectorNode( vector,
-          graph,
-          vectorAdditionModel.valuesVisibleProperty,
-          vectorAdditionModel.angleVisibleProperty );
-        vectorContainer.addChild( vectorNode );
+        // Step 2: A call to the Scene Node is made, passing the created Vector to create the subsequent views
+        //----------------------------------------------------------------------------------------
 
-        // Activate the body drag
-        vectorNode.bodyDragListener.press( event, vectorNode );
+        sceneNode.registerVector( vector, vectorSet, event );
 
         // Change the visibility to allow or not allow infinite slots
         iconNode.visible = options.isInfinite;
 
         //----------------------------------------------------------------------------------------
-        // Add the removal listener for when the vector is removed to remove the node.
-        const removeVectorNodeListener = removedVector => {
-          if ( removedVector === vector ) {
-            removedVector.isOnGraphProperty.value = false;
-            vectorNode.dispose();
+        // Step 3: Once the Vector indicates the Vector was dropped outside the Graph, animate and
+        // dispose the Vector, signaling to the Scene Node to dispose of the views.
+        //----------------------------------------------------------------------------------------
+
+        const animateVectorBackListener = animateBack => {
+
+          // Get the position of the icon node relative to the scene. Used to animate the vector to the icon position.
+          const iconPosition = graph.modelViewTransformProperty.value.viewToModelBounds( sceneNode.boundsOf( iconNode ) ).center;
+          animateBack && vector.animateToPoint( iconPosition, iconComponents, () => {
             iconNode.visible = true;
-            vectorSet.vectors.removeItemRemovedListener( removeVectorNodeListener );
-          }
+
+            // Remove the vector model
+            vectorSet.vectors.remove( vector );
+            vector.dispose();
+
+          } );
         };
-        vectorSet.vectors.addItemRemovedListener( removeVectorNodeListener );
+        vector.animateBackProperty.link( animateVectorBackListener );
+
 
         //----------------------------------------------------------------------------------------
-        // Observe when the vector node says its time to animate back.
-        vectorNode.animateBackProperty.link( ( animateBack ) => {
-
-          if ( animateBack ) {
-
-            // Get the location of the icon node relative to the screen view
-            const iconPosition = modelViewTransformProperty.value.viewToModelBounds(
-              parentScreenView.boundsOf( iconNode ) ).center;
-
-            // Create the listener to reset the slot when animation finished
-            const animationFinishedListener = () => {
-              iconNode.visible = true;
-
-              // Remove the vector model
-              vectorSet.vectors.remove( vector );
-              vector.dispose();
-            };
-            vector.animateToPoint( iconPosition, iconComponents, animationFinishedListener );
+        // Observe when the vector is removed to unlink listeners
+        const removeVectorListener = removedVector => {
+          if ( removedVector === vector ) {
+            vector.animateBackProperty.unlink( animateVectorBackListener );
+            vectorSet.vectors.removeItemRemovedListener( removeVectorListener );
           }
-        } );
+        };
+        vectorSet.vectors.addItemRemovedListener( removeVectorListener );
 
-      }, {
-        allowTouchSnag: true
-      } ) );
+
+      }, { allowTouchSnag: true } ) );
     }
   }
 
