a9f2cb2548345bee3b3f00e93655ab923ddda040
Brandon Li <brandon.li820@icloud.com>
2019-08-05 14:59:55 -0600

    #81, clean up equation types

--- js/equation/model/EquationTypes.js
+++ js/equation/model/EquationTypes.js
@@ -1,7 +1,7 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * Enumeration of the equation types.
+ * Enumeration of the possible 'equation types.'
  *
  * @author Brandon Li
  */
@@ -13,13 +13,21 @@ define( require => {
   const Enumeration = require( 'PHET_CORE/Enumeration' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
 
-  /**
-   * There are three different types of equations:
-   * 'ADDITION': a + b = c. Adding two vectors to get to a third.
-   * 'SUBTRACTION': a - b = c. Subtracting two vectors to get a third
-   * 'NEGATION': a + b = -c or a + b + c = 0; Negating the sum of the first two vectors.
-   */
-  const EquationTypes = new Enumeration( [ 'ADDITION', 'SUBTRACTION', 'NEGATION' ] );
+  const EquationTypes = new Enumeration( [
+
+    'ADDITION',     // Adding two vectors to get a third.
+                    // Shown as 'A + B = C' || 'D + E = F' on the Equation Types Radio Buttons
+
+    'SUBTRACTION',  // Subtracting a vector from another to get a third.
+                    // Shown as 'A - B = C' || 'D - E = F' on the Equation Types Radio Buttons
+
+
+    'NEGATION'      // Negating the sum of two vectors to get a third.
+                    // Derived from '-( A + B ) = C'. Simplified to 'A + B + C = 0'
+                    //
+                    // Shown as 'A + B + C = 0' || 'D + E + F = 0' on the Equation Types Radio Buttons
+
+  ] );
 
   return vectorAddition.register( 'EquationTypes', EquationTypes );
 } );
