d94d1b7d7f066d8cea927d64d34af6268ba2c80a
Brandon Li <brandon.li820@icloud.com>
2019-08-13 13:05:35 -0600

    (preliminary) clean up equation scene node

--- js/equation/view/EquationSceneNode.js
+++ js/equation/view/EquationSceneNode.js
@@ -1,13 +1,13 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * A Scene Node specific to the 'Equation' screen. See ../common/view/SceneNode for context on Scene
+ * A Scene Node specific to the 'Equation' screen. See ../common/view/SceneNode.js for context on Scene Node
  *
  * 'Is A' relationship with Scene Node but adds:
  *  - a Equation Selector Radio Button Group
  *  - a Coefficient Selector Panel for each Equation Type
  *  - a Base Vector Accordion Box
- *  - Base Vectors and their components,
+ *  - 'register' Vectors and add their Base Vectors and their components
  *  - Disables the Eraser button
  *
  * @author Brandon Li
@@ -21,27 +21,11 @@ define( require => {
   const CoefficientSelectorPanel = require( 'VECTOR_ADDITION/equation/view/CoefficientSelectorPanel' );
   const EquationTypes = require( 'VECTOR_ADDITION/equation/model/EquationTypes' );
   const EquationTypesRadioButtonGroup = require( 'VECTOR_ADDITION/equation/view/EquationTypesRadioButtonGroup' );
+  const merge = require( 'PHET_CORE/merge' );
   const SceneNode = require( 'VECTOR_ADDITION/common/view/SceneNode' );
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
   const VectorAdditionColors = require( 'VECTOR_ADDITION/common/VectorAdditionColors' );
   const VectorNode = require( 'VECTOR_ADDITION/common/view/VectorNode' );
-
-  // constants
-
-  // Passed to the super class (SceneNode) as the 'options' parameter
-  const SCENE_NODE_OPTIONS = {
-    includeEraser: false,               // Disable the Eraser button
-    includeBaseVectors: true,           // Create a Vector Layer for Base Vectors and a layer for their components!
-    sumNodeOptions: {
-      fill: VectorAdditionColors.EQUATION_SUM_FILL
-    },
-    vectorValuesPanelOptions: {
-      spacingMajor: 25,
-      contentFixedWidth: 470
-    }
-  };
-  const BASE_VECTOR_OPACITY = 0.38;
-  const PANEL_CENTER_Y = 100;
 
   class EquationSceneNode extends SceneNode {
 
@@ -51,79 +35,109 @@ define( require => {
      * @param {BooleanProperty} angleVisibleProperty
      * @param {BooleanProperty} gridVisibleProperty
      * @param {EnumerationProperty.<ComponentStyles>} componentStyleProperty
+     * @param {Object} [options]
      */
     constructor( equationGraph,
                  valuesVisibleProperty,
                  angleVisibleProperty,
                  gridVisibleProperty,
-                 componentStyleProperty ) {
+                 componentStyleProperty,
+                 options ) {
+
+
+      options = merge( {
+
+        // specific to this class
+        baseVectorOpacity: 0.38,  // {number} opacity of Base Vector Nodes and their components
+        panelCenterY: 100,        // {number} centerY of the equationTypesRadioButtonGroup and the
+                                  //          coefficientSelectorPanel
+
+
+        // super-class options
+        includeEraser: false,
+        sumNodeOptions: {
+          fill: VectorAdditionColors.EQUATION_SUM_FILL
+        },
+        vectorValuesPanelOptions: {
+          spacingMajor: 25,
+          contentFixedWidth: 470
+        }
+
+      }, options );
+
 
       super( equationGraph,
         valuesVisibleProperty,
         angleVisibleProperty,
         gridVisibleProperty,
         componentStyleProperty,
-        SCENE_NODE_OPTIONS);
+        options );
 
-      const graphViewBounds = equationGraph.modelViewTransformProperty.value.modelToViewBounds( equationGraph.graphModelBounds );
 
       //----------------------------------------------------------------------------------------
-      // Add the equation types radio button Group
+      // Add the "Equation Selector Radio Button Group"
+
+      // convenience reference
+      const graphViewBounds = equationGraph.modelViewTransformProperty.value.modelToViewBounds( equationGraph.graphModelBounds );
 
       const equationTypesRadioButtonGroup = new EquationTypesRadioButtonGroup( equationGraph.equationTypeProperty,
         equationGraph.vectorSet.symbols, {
-          centerY: PANEL_CENTER_Y,
+          centerY: options.panelCenterY,
           right: graphViewBounds.maxX
         } );
 
       this.addChild( equationTypesRadioButtonGroup );
-      equationTypesRadioButtonGroup.moveToBack();
+      equationTypesRadioButtonGroup.moveToBack(); // move to back to keep Vector Containers on top in the super class
 
 
-      // Add the coefficient for each equation type
+      //----------------------------------------------------------------------------------------
+      // Add a Coefficient Selector Panel for each Equation Type
       EquationTypes.VALUES.forEach( equationType => {
 
         const coefficientSelectorPanel = new CoefficientSelectorPanel( equationGraph.vectorSet, equationType, {
-          centerY: PANEL_CENTER_Y
+          centerY: options.panelCenterY
         } );
 
-        // Doesn't need to be unlinked since the coefficientSelectorPanel is never disposed
+        // Doesn't need to be unlinked since the coefficientSelectorPanel and the scene is never disposed
         equationGraph.equationTypeProperty.link( () => {
           coefficientSelectorPanel.visible = equationType === equationGraph.equationTypeProperty.value;
         } );
 
         this.addChild( coefficientSelectorPanel );
-        coefficientSelectorPanel.moveToBack();
+        coefficientSelectorPanel.moveToBack(); // move to back to keep Vector Containers on top in the super class
 
       } );
 
       //----------------------------------------------------------------------------------------
-      // Add the base vectors
-
-      equationGraph.vectorSet.vectors.forEach( ( vector ) => {
-
-        const baseVector = new VectorNode( vector.baseVector, equationGraph, valuesVisibleProperty, angleVisibleProperty, {
-          opacity: BASE_VECTOR_OPACITY
-        } );
-
-        equationGraph.baseVectorsVisibleProperty.linkAttribute( baseVector, 'visible' );
-
-        this.baseVectorContainer.addChild( baseVector );
-      } );
-
+      // Add a Base Vector Accordion Box
 
       const baseVectorsAccordionBox = new BaseVectorsAccordionBox( equationGraph.baseVectorsVisibleProperty,
         equationGraph.coordinateSnapMode,
         equationGraph.vectorSet );
 
-
-
-
-
       // Add the base vectors accordion box (semi-global)
       this.addChild( baseVectorsAccordionBox );
 
       baseVectorsAccordionBox.moveToBack();
+
+
+      //----------------------------------------------------------------------------------------
+      // 'Register' Vectors and add their Base Vectors and their components
+      equationGraph.vectorSet.vectors.forEach( equationVector => {
+
+        // register the vector to create the Nodes
+        this.registerVector( equationVector, equationGraph.vectorSet );
+
+
+        // TODO: create add the components
+        const baseVector = new VectorNode( equationVector.baseVector, equationGraph, valuesVisibleProperty, angleVisibleProperty, {
+          opacity: options.baseVectorOpacity
+        } );
+        equationGraph.baseVectorsVisibleProperty.linkAttribute( baseVector, 'visible' );
+
+        this.baseVectorContainer.addChild( baseVector );
+      } );
+
     }
   }
 
