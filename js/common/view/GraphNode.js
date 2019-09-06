9fb475cb771fd4d0200d409825b0989c5a796ed9
Chris Malley <cmalley@pixelzoom.com>
2019-09-06 11:58:55 -0400

    GraphNode optimizations and cleanup, #168

--- js/common/view/GraphNode.js
+++ js/common/view/GraphNode.js
@@ -1,8 +1,8 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * View for the graph, including its axes, grid, and origin dot. The origin dot is draggable, which changes the
- * modelViewTransformProperty of the graph.
+ * View for the graph, including its grid, axes, ticks, and origin dot.
+ * The origin dot is draggable, which changes the modelViewTransformProperty of the graph.
  *
  * @author Martin Veillette
  * @author Chris Malley (PixelZoom, Inc.)
@@ -86,15 +86,16 @@ define( require => {
           stroke: VectorAdditionColors.GRAPH_MINOR_LINE_COLOR,
           lineWidth: MINOR_GRID_LINE_WIDTH
         } ),
-        new MajorAndMinorGridLines( graph, graphViewBounds, gridVisibilityProperty )
+        new MajorAndMinorGridLines( graph, graphViewBounds, gridVisibilityProperty ),
+        new TicksNode( graph )
       ];
 
       // Create axes as needed, based on graph orientation
       if ( graph.orientation !== GraphOrientations.VERTICAL ) {
-        children.push( new XAxisNode( graph ) );
+        children.push( new XAxisNode( graph, graphViewBounds ) );
       }
       if ( graph.orientation !== GraphOrientations.HORIZONTAL ) {
-        children.push( new YAxisNode( graph ) );
+        children.push( new YAxisNode( graph, graphViewBounds ) );
       }
 
       children.push( new OriginCircle( graph ) );
@@ -207,233 +208,132 @@ define( require => {
   }
 
   /**
-   * Abstract base class for axes.
+   * Draws the x axis.
    */
-  class AxisNode extends Node {
+  class XAxisNode extends Node {
 
     /**
-     * @param {Graph} graph - the model for the graph
-     * @param {string} axisLabelString - the label for the axis
+     * @param {Graph} graph
+     * @param {Bounds2} graphViewBounds
      */
-    constructor( graph, axisLabelString ) {
+    constructor( graph, graphViewBounds ) {
 
-      super();
+      const arrowNode = new ArrowNode(
+        graphViewBounds.minX - AXES_ARROW_X_EXTENSION, 0,
+        graphViewBounds.maxX + AXES_ARROW_X_EXTENSION, 0,
+        VectorAdditionConstants.AXES_ARROW_OPTIONS
+      );
 
-      // @protected {ArrowNode} axisArrow - Create an double arrow that represents the axis. Arbitrary length since the
-      // positioning and extent is different for xAxis and yAxis.
-      this.axisArrow = new ArrowNode( 0, 0, 1, 1, VectorAdditionConstants.AXES_ARROW_OPTIONS );
+      const axisLabel = new Text( symbolXString, _.extend( {}, AXES_TEXT_OPTIONS, {
+        left: arrowNode.right + 6,
+        centerY: arrowNode.centerY
+      } ) );
 
-      // @protected {Text} axisLabel - Create a label next to the axis arrow. Label positioning is different for the
-      // different axes.
-      this.axisLabel = new Text( axisLabelString, AXES_TEXT_OPTIONS );
+      super( {
+        children: [ arrowNode, axisLabel ]
+      } );
 
-      // Create a path that represents the ticks along the axis with an empty shape which will be updated when the
-      // modelViewTransform is updated.
-      const tickMarksPath = new Path( new Shape(), TICKS_OPTIONS );
-
-      const children = [ this.axisArrow, this.axisLabel, tickMarksPath ];
-
-      // If the graph is not 2 dimensional, label the origin
-      this.originText = null;
-      if ( graph.orientation !== GraphOrientations.TWO_DIMENSIONAL ) {
-
-        // @public {Text} originText - create a label for the origin that will be moved when the modelViewTransform is
-        // updated.
-        this.originText = new Text( '0', TICK_TEXT_OPTIONS );
-        children.push( this.originText );
-      }
-
-      this.setChildren( children );
-
-      // Observe changes to the modelViewTransform and update the axis when changed.
-      graph.modelViewTransformProperty.link( ( modelViewTransform ) => {
-
-        // Graph bounds in view coordinates
-        const graphViewBounds = modelViewTransform.modelToViewBounds( graph.graphModelBounds );
-        
-        // Position of the origin in view coordinates
-        const graphViewOrigin = modelViewTransform.modelToViewPosition( Vector2.ZERO );
-
-        // Update the axis
-        this.updateAxisArrow( graphViewBounds, graphViewOrigin );
-
-        // Update labels on the axis
-        this.updateAxisLabels( graphViewOrigin );
-
-        // Update tick marks
-        tickMarksPath.setShape( this.createTicksShape( graph.graphModelBounds, modelViewTransform ).makeImmutable() );
+      // When the origin moves, adjust the position of the axis.
+      // No need to unlink since GraphNodes exist for the lifetime of the sim.
+      graph.modelViewTransformProperty.link( modelViewTransform => {
+        this.y = modelViewTransform.modelToViewY( 0 );
       } );
     }
+  }
+
+  /**
+   * Draws the y axis.
+   */
+  class YAxisNode extends Node {
 
     /**
-     * Updates the location of the arrow
-     * @param {Bounds2} graphViewBounds - the bounds of the grid in view coordinates
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     * @abstract
+     * @param {Graph} graph
+     * @param {Bounds2} graphViewBounds
      */
-    updateAxisArrow( graphViewBounds, graphViewOrigin ) {
-      throw new Error( 'abstract method must be implemented by subclass' );
-    }
+    constructor( graph, graphViewBounds ) {
 
-    /**
-     * Updates the location of the labels (axis label and origin tick label)
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     * @abstract
-     */
-    updateAxisLabels( graphViewOrigin ) {
-      throw new Error( 'abstract method must be implemented by subclass' );
-    }
+      const arrowNode = new ArrowNode(
+        0, graphViewBounds.minY - AXES_ARROW_Y_EXTENSION,
+        0, graphViewBounds.maxY + AXES_ARROW_Y_EXTENSION,
+        VectorAdditionConstants.AXES_ARROW_OPTIONS
+      );
 
-    /**
-     * Gets a new shape for the updated axis ticks
-     * @param {Bounds2} graphModelBounds - the bounds of the grid in model coordinates
-     * @param {ModelViewTransform2} modelViewTransform - the new modelViewTransform
-     * @returns {Shape} the new axis ticks shape in View coordinates
-     * @protected
-     * @abstract
-     */
-    createTicksShape( graphModelBounds, modelViewTransform ) {
-      throw new Error( 'abstract method must be implemented by subclass' );
+      const axisLabel = new Text( symbolYString, _.extend( {}, AXES_TEXT_OPTIONS, {
+        centerX: arrowNode.centerX,
+        bottom: arrowNode.top - 3
+      } ) );
+
+      super( {
+        children: [ arrowNode, axisLabel ]
+      } );
+
+      // When the origin moves, adjust the position of the axis.
+      // No need to unlink since GraphNodes exist for the lifetime of the sim.
+      graph.modelViewTransformProperty.link( modelViewTransform => {
+        this.x = modelViewTransform.modelToViewX( 0 );
+      } );
     }
   }
 
-  class XAxisNode extends AxisNode {
+  /**
+   * Draws the tick marks and labels.
+   */
+  class TicksNode extends Node {
 
     /**
-     * @param {Graph} graph - the model for the graph
+     * @param {Graph} graph
      */
     constructor( graph ) {
-      super( graph, symbolXString );
-    }
 
-    /**
-     * Updates the location of the arrow
-     * @param {Bounds2} graphViewBounds - the bounds of the grid in view coordinates
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     */
-    updateAxisArrow( graphViewBounds, graphViewOrigin ) {
-      this.axisArrow.setTailAndTip(
-        graphViewBounds.minX - AXES_ARROW_X_EXTENSION,
-        graphViewOrigin.y,
-        graphViewBounds.maxX + AXES_ARROW_X_EXTENSION,
-        graphViewOrigin.y
-      );
-    }
+      const tickMarksPath = new Path( new Shape(), TICKS_OPTIONS );
+      const tickLabelsParent = new Node();
+      const originLabel = new Text( '0', TICK_TEXT_OPTIONS );
 
-    /**
-     * Updates the location of the labels
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     */
-    updateAxisLabels( graphViewOrigin ) {
+      super( {
+        children: [ tickMarksPath, tickLabelsParent ]
+      } );
 
-      // Update the label that is to the right of the axis
-      this.axisLabel.left = this.axisArrow.right + 6;
-      this.axisLabel.centerY = graphViewOrigin.y;
+      graph.modelViewTransformProperty.link( modelViewTransform => {
 
-      // Update the origin label
-      if ( this.originText ) {
-        this.originText.centerX = graphViewOrigin.x;
-        this.originText.top = graphViewOrigin.y + 20;
-      }
-    }
+        const shape = new Shape();
 
-    /**
-     * Creates the Shape for the x-axis tick marks.
-     * @param {Bounds2} graphModelBounds - the bounds of the grid in model coordinates
-     * @param {ModelViewTransform2} modelViewTransform - the new modelViewTransform
-     * @returns {Shape} the new axis ticks shape in View coordinates
-     * @protected
-     */
-    createTicksShape( graphModelBounds, modelViewTransform ) {
+        // x-axis ticks
+        if ( graph.orientation !== GraphOrientations.VERTICAL ) {
+          const firstXTick = graph.graphModelBounds.minX - ( graph.graphModelBounds.minX % MAJOR_TICK_SPACING );
+          for ( let xValue = firstXTick; xValue <= graph.graphModelBounds.maxX; xValue = xValue + MAJOR_TICK_SPACING ) {
+            const tickLength = ( xValue === 0 ) ? ORIGIN_TICK_LENGTH : TICK_LENGTH; // origin tick is different
+            shape.moveTo( xValue, -tickLength / 2 ).verticalLineTo( tickLength / 2 );
+          }
+        }
 
-      // Create ticks along the axis
-      const xAxisTicksShape = new Shape();
+        // y-axis ticks
+        if ( graph.orientation !== GraphOrientations.HORIZONTAL ) {
+          const firstYTick = graph.graphModelBounds.minY - ( graph.graphModelBounds.minY % MAJOR_TICK_SPACING );
+          for ( let yValue = firstYTick; yValue <= graph.graphModelBounds.maxY; yValue = yValue + MAJOR_TICK_SPACING ) {
+            const tickLength = ( yValue === 0 ) ? ORIGIN_TICK_LENGTH : TICK_LENGTH; // origin tick is different
+            shape.moveTo( -tickLength / 2, yValue ).horizontalLineTo( tickLength / 2 );
+          }
+        }
 
-      const firstTick = graphModelBounds.minX - ( graphModelBounds.minX % MAJOR_TICK_SPACING );
-      
-      // x-axis major ticks
-      for ( let xValue = firstTick; xValue <= graphModelBounds.maxX; xValue = xValue + MAJOR_TICK_SPACING ) {
+        tickMarksPath.shape = modelViewTransform.modelToViewShape( shape );
 
-        // The origin has a longer tick
-        const tickLength = ( xValue === 0 ) ? ORIGIN_TICK_LENGTH : TICK_LENGTH;
+        // Origin tick label
+        tickLabelsParent.removeAllChildren();
+        if ( graph.orientation !== GraphOrientations.TWO_DIMENSIONAL ) {
 
-        xAxisTicksShape.moveTo( xValue, -tickLength / 2 ).verticalLineTo( tickLength / 2 );
-      }
+          const viewOrigin = modelViewTransform.modelToViewPosition( Vector2.ZERO );
 
-      return modelViewTransform.modelToViewShape( xAxisTicksShape );
-    }
-  }
-
-  class YAxisNode extends AxisNode {
-    
-    /**
-     * @param {Graph} graph - the model for the graph
-     */
-    constructor( graph ) {
-      super( graph, symbolYString );
-    }
-
-    /**
-     * Updates the location of the arrow
-     * @param {Bounds2} graphViewBounds - the bounds of the grid in view coordinates
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     */
-    updateAxisArrow( graphViewBounds, graphViewOrigin ) {
-      this.axisArrow.setTailAndTip(
-        graphViewOrigin.x,
-        graphViewBounds.minY - AXES_ARROW_Y_EXTENSION,
-        graphViewOrigin.x,
-        graphViewBounds.maxY + AXES_ARROW_Y_EXTENSION
-      );
-    }
-
-    /**
-     * Updates the location of the labels
-     * @param {Vector2} graphViewOrigin - the origin location in view coordinates
-     * @protected
-     */
-    updateAxisLabels( graphViewOrigin ) {
-
-      // Update the label that is above the axis
-      this.axisLabel.centerX = graphViewOrigin.x;
-      this.axisLabel.bottom = this.axisArrow.top - 3;
-
-      // Update the origin label
-      if ( this.originText ) {
-        this.originText.centerY = graphViewOrigin.y;
-        this.originText.right = graphViewOrigin.x - 20;
-      }
-    }
-
-    /**
-     * Creates the Shape for the y-axis tick marks.
-     * @param {Bounds2} graphModelBounds - the bounds of the grid in model coordinates
-     * @param {ModelViewTransform2} modelViewTransform - the new modelViewTransform
-     * @returns {Shape} the new axis ticks shape in View coordinates
-     * @protected
-     */
-    createTicksShape( graphModelBounds, modelViewTransform ) {
-
-      // create ticks along the axis
-      const yAxisTicksShape = new Shape();
-
-      const firstTick = graphModelBounds.minY - ( graphModelBounds.minY % MAJOR_TICK_SPACING );
-
-      // y-axis major ticks
-      for ( let yValue = firstTick; yValue <= graphModelBounds.maxY; yValue = yValue + MAJOR_TICK_SPACING ) {
-
-        // The origin has a longer tick
-        const tickLength = ( yValue === 0 ) ? ORIGIN_TICK_LENGTH : TICK_LENGTH;
-
-        yAxisTicksShape.moveTo( -tickLength / 2, yValue ).horizontalLineTo( tickLength / 2 );
-      }
-
-      return modelViewTransform.modelToViewShape( yAxisTicksShape );
+          tickLabelsParent.addChild( originLabel );
+          if ( graph.orientation === GraphOrientations.HORIZONTAL ) {
+            originLabel.centerX = viewOrigin.x;
+            originLabel.top = viewOrigin.y + 20;
+          }
+          else {
+            originLabel.right = viewOrigin.x - 20;
+            originLabel.centerY = viewOrigin.y;
+          }
+        }
+      } );
     }
   }
 
