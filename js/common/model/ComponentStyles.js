a2121da452f6e75e46a2baee46551fc687aab4c9
Chris Malley <cmalley@pixelzoom.com>
2019-09-19 08:37:17 -0600

    doc cleanup

--- js/common/model/ComponentStyles.js
+++ js/common/model/ComponentStyles.js
@@ -14,14 +14,14 @@ define( require => {
   const vectorAddition = require( 'VECTOR_ADDITION/vectorAddition' );
 
   const ComponentStyles = new Enumeration( [
-    'INVISIBLE',        // Component Vectors are not displayed at all
+    'INVISIBLE',        // Component vectors are not displayed at all
 
-    'PARALLELOGRAM',    // Component Vector's initial points and the original vector's initial points coincide
+    'PARALLELOGRAM',    // Component vector's initial points and the original vector's initial points coincide
 
-    'TRIANGLE',         // Component Vectors are displayed head to tail, such that the component vectors
+    'TRIANGLE',         // Component vectors are displayed head to tail, such that the component vectors
                         // align to create a right triangle with the original vector
 
-    'ON_AXIS'           // Component Vectors are displayed on the x and y axes
+    'ON_AXIS'           // Component vectors are displayed on the x and y axes
   ] );
 
   return vectorAddition.register( 'ComponentStyles', ComponentStyles );
