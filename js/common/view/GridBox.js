a6ff3d3031e5043e614d15849a0ab19f8e1518de
Chris Malley <cmalley@pixelzoom.com>
2019-10-03 10:57:35 -0600

    add GridBox support for mutable contents, #190

--- js/common/view/GridBox.js
+++ js/common/view/GridBox.js
@@ -26,12 +26,10 @@ define( require => {
   class GridBox extends Node {
 
     /**
-     * @param {Node[]} nodes - the Nodes in the grid, in row-major order
+     * @param {Node[]} contents - the contentss of the grid, in row-major order
      * @param {Object} [options]
      */
-    constructor( nodes, options ) {
-
-      assert && assert( Array.isArray( nodes ), 'invalid nodes' );
+    constructor( contents, options ) {
 
       options = _.extend( {
         columns: 2, // {number} number of columns
@@ -48,6 +46,47 @@ define( require => {
       assert && assert( _.includes( X_ALIGN_VALUES, options.xAlign ), `invalid xAlign: ${options.xAlign}` );
       assert && assert( _.includes( Y_ALIGN_VALUES, options.yAlign ), `invalid xAlign: ${options.yAlign}` );
 
+      const vBox = new VBox( {
+        spacing: options.ySpacing,
+        align: 'left'
+      } );
+
+      assert && assert( !options.children, 'GridBox sets children' );
+      options.children = [ vBox ];
+
+      super( options );
+
+      // @private
+      this.contents = contents;
+      this.vBox = vBox;
+      this.columns = options.columns;
+      this.xSpacing = options.xSpacing;
+      this.xAlign = options.xAlign;
+      this.yAlign = options.yAlign;
+
+      this.setContents( contents );
+    }
+
+    /**
+     * Gets the contents of the grid, in row-major order.
+     * @returns {Node[]}
+     * @public
+     */
+    getContents() {
+      return this.contents;
+    }
+
+    /**
+     * Sets the contents of the grid, in row-major order.
+     * @param {Node[]} contents - the contents of the grid, in row-major order
+     * @public
+     */
+    setContents( contents ) {
+      assert && assert( Array.isArray( contents ), 'contents must be an Array' );
+      assert && assert( _.every( contents, element => element instanceof Node ), 'every element in contents must be a Node' );
+
+      this.contents = contents;
+
       // Use an AlignGroup to ensure that every Node in the grid has the same effective bounds.
       const alignGroup = new AlignGroup( {
         matchHorizontal: true,
@@ -57,37 +96,28 @@ define( require => {
       // Process the Nodes, in row-major order
       const vBoxChildren = [];
       let i = 0;
-      while ( i < nodes.length ) {
+      while ( i < contents.length ) {
 
         const hBoxChildren = [];
-        for ( let column = 0; column < options.columns && i < nodes.length; column++ ) {
+        for ( let column = 0; column < this.columns && i < contents.length; column++ ) {
 
           // Wrap each child in an AlignBox, so that every Node in the grid has the same effective bounds,
           // and is aligned within those bounds as specified by options xAlign and yAlign.
-          hBoxChildren.push( new AlignBox( nodes[ i++ ], {
+          hBoxChildren.push( new AlignBox( contents[ i++ ], {
             group: alignGroup,
-            xAlign: options.xAlign,
-            yAlign: options.yAlign
+            xAlign: this.xAlign,
+            yAlign: this.yAlign
           } ) );
         }
 
         vBoxChildren.push( new HBox( {
           children: hBoxChildren,
-          spacing: options.xSpacing,
+          spacing: this.xSpacing,
           align: 'origin'
         } ) );
       }
 
-      const vBox = new VBox( {
-        children: vBoxChildren,
-        spacing: options.ySpacing,
-        align: 'left'
-      } );
-
-      assert && assert( !options.children, 'GridBox sets children' );
-      options.children = [ vBox ];
-
-      super( options );
+      this.vBox.children = vBoxChildren;
     }
   }
 
