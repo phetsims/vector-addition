9783180217a7343837ef5135da045603d4f9c532
Chris Malley <cmalley@pixelzoom.com>
2019-10-28 22:09:26 -0600

    factor out ZERO_MAGNITUDE_THRESHOLD constant, #260

--- js/common/view/RootVectorNode.js
+++ js/common/view/RootVectorNode.js
@@ -38,6 +38,9 @@ define( require => {
     VectorAdditionConstants.DEFAULT_GRAPH_BOUNDS.height - VectorAdditionConstants.VECTOR_TAIL_DRAG_MARGIN
   ).magnitude;
 
+  // magnitude smaller than this value is effectively zero
+  const ZERO_MAGNITUDE_THRESHOLD = 1E-10;
+
   class RootVectorNode extends Node {
 
     /**
@@ -167,7 +170,7 @@ define( require => {
 
       // If the magnitude is effectively 0, center the label on the vector's position.
       // See https://github.com/phetsims/vector-addition/issues/260
-      if ( rootVector.magnitude < 1E-10 ) {
+      if ( rootVector.magnitude < ZERO_MAGNITUDE_THRESHOLD ) {
         this.labelNode.center = modelViewTransform.modelToViewDelta( Vector2.ZERO );
         return;
       }
