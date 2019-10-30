dd3566487974a28ce8ec53e1eee52761147d2069
Chris Malley <cmalley@pixelzoom.com>
2019-10-30 17:14:32 -0600

    revise QA credits, #98

--- js/common/VectorAdditionConstants.js
+++ js/common/VectorAdditionConstants.js
@@ -52,7 +52,7 @@ define( require => {
       leadDesign: 'Michael Dubson, Amy Rouinfar',
       softwareDevelopment: 'Brandon Li, Martin Veillette, Chris Malley (PixelZoom, Inc.)',
       team: 'Diana L\u00f3pez Tavares, Amanda McGarry, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Megan Lai, Katie Woessner'
+      qualityAssurance: 'Jaspe Arias, Logan Bray, Megan Lai, Liam Mulhall, Kathryn Woessner'
     },
 
     //----------------------------------------------------------------------------------------
