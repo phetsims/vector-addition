5bd6553596d10971998191e83e1c1b1c3c9d9504
samreid <reids@colorado.edu>
2020-02-27 12:08:00 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/vector-addition-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import vectorAddition from './vectorAddition.js';
+
+const vectorAdditionStrings = getStringModule( 'VECTOR_ADDITION' );
+
+vectorAddition.register( 'vectorAdditionStrings', vectorAdditionStrings );
+
+export default vectorAdditionStrings;
