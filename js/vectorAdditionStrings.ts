6799acb97ce8c05f5fa0dca911c271daa30a3eb5
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:51 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/vectorAdditionStrings.ts
@@ -0,0 +1,37 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import vectorAddition from './vectorAddition.js';
+
+type StringsType = {
+  'vector-addition': {
+    'title': string
+  },
+  'screen': {
+    'equations': string,
+    'explore1D': string,
+    'explore2D': string,
+    'lab': string
+  },
+  'sum': string,
+  'values': string,
+  'components': string,
+  'noVectorSelected': string,
+  'vectorValues': string,
+  'symbol': {
+    'x': string,
+    'y': string
+  },
+  'baseVectors': string,
+  'equation': string
+};
+
+const vectorAdditionStrings = getStringModule( 'VECTOR_ADDITION' ) as StringsType;
+
+vectorAddition.register( 'vectorAdditionStrings', vectorAdditionStrings );
+
+export default vectorAdditionStrings;
