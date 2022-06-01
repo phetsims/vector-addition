071846de1186f022b84343a6c230c62cf5daef9f
phet-dev <phetadmin@gmail.com>
2022-06-01 03:46:43 -0600

    update copyright dates from daily grunt work

--- js/common/view/VectorCheckbox.js
+++ js/common/view/VectorCheckbox.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * VectorCheckbox is a checkbox that is labeled with a vector symbol and vector arrow.
