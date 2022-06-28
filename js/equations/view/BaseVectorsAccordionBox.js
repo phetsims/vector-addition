97c28b2a7b6ba088a4df7c735c4fd4cef555b810
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-28 16:09:23 -0600

    Move NumberPicker to sun, see: https://github.com/phetsims/sun/issues/436

--- js/equations/view/BaseVectorsAccordionBox.js
+++ js/equations/view/BaseVectorsAccordionBox.js
@@ -18,7 +18,7 @@ import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import merge from '../../../../phet-core/js/merge.js';
 import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
-import NumberPicker from '../../../../scenery-phet/js/NumberPicker.js';
+import NumberPicker from '../../../../sun/js/NumberPicker.js';
 import { HBox } from '../../../../scenery/js/imports.js';
 import { HStrut } from '../../../../scenery/js/imports.js';
 import { Node } from '../../../../scenery/js/imports.js';
--- js/equations/view/EquationTypeNode.js
+++ js/equations/view/EquationTypeNode.js
@@ -9,7 +9,7 @@
 import Property from '../../../../axon/js/Property.js';
 import merge from '../../../../phet-core/js/merge.js';
 import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
-import NumberPicker from '../../../../scenery-phet/js/NumberPicker.js';
+import NumberPicker from '../../../../sun/js/NumberPicker.js';
 import { Node } from '../../../../scenery/js/imports.js';
 import { Text } from '../../../../scenery/js/imports.js';
 import VectorAdditionConstants from '../../common/VectorAdditionConstants.js';
