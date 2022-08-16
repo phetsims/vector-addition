3ce21a1ccae885dffd61a6ca9474d62af3134b58
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/vectorAdditionStrings.ts
+++ js/vectorAdditionStrings.ts
@@ -5,29 +5,44 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import vectorAddition from './vectorAddition.js';
 
 type StringsType = {
   'vector-addition': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'equations': string;
+    'equationsProperty': TReadOnlyProperty<string>;
     'explore1D': string;
+    'explore1DProperty': TReadOnlyProperty<string>;
     'explore2D': string;
+    'explore2DProperty': TReadOnlyProperty<string>;
     'lab': string;
+    'labProperty': TReadOnlyProperty<string>;
   };
   'sum': string;
+  'sumProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'components': string;
+  'componentsProperty': TReadOnlyProperty<string>;
   'noVectorSelected': string;
+  'noVectorSelectedProperty': TReadOnlyProperty<string>;
   'vectorValues': string;
+  'vectorValuesProperty': TReadOnlyProperty<string>;
   'symbol': {
     'x': string;
+    'xProperty': TReadOnlyProperty<string>;
     'y': string;
+    'yProperty': TReadOnlyProperty<string>;
   };
   'baseVectors': string;
+  'baseVectorsProperty': TReadOnlyProperty<string>;
   'equation': string;
+  'equationProperty': TReadOnlyProperty<string>;
 };
 
 const vectorAdditionStrings = getStringModule( 'VECTOR_ADDITION' ) as StringsType;
