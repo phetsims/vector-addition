8aeb949e0ad9f2675c3a34094437da9900e49b87
phet-dev <phetadmin@gmail.com>
2023-01-14 05:02:24 -0700

    update copyright dates from daily grunt work

--- js/explore1D/view/GraphOrientationRadioButtonGroup.js
+++ js/explore1D/view/GraphOrientationRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Radio button group for switching between the Vertical and Horizontal graph.
