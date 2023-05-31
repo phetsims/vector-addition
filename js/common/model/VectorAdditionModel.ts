2c5aa617a66a4bb6ed7b3a9855b7353e6c226f6f
pixelzoom <cmalley@pixelzoom.com>
2023-05-30 18:54:07 -0600

    VectorAdditionModel implements TModel

--- js/common/model/VectorAdditionModel.ts
+++ js/common/model/VectorAdditionModel.ts
@@ -1,7 +1,7 @@
 // Copyright 2019-2023, University of Colorado Boulder
 
 /**
- * Root class (to be subclassed) for the top-level model of every screen.
+ * VectorAdditionModel is the base class for the top-level model of every screen.
  *
  * @author Martin Veillette
  */
@@ -10,8 +10,9 @@ import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import vectorAddition from '../../vectorAddition.js';
 import ComponentVectorStyles from './ComponentVectorStyles.js';
+import TModel from '../../../../joist/js/TModel.js';
 
-export default class VectorAdditionModel {
+export default class VectorAdditionModel implements TModel {
 
   // the representation (style) used to display component vectors
   public readonly componentStyleProperty: EnumerationProperty<ComponentVectorStyles>;
