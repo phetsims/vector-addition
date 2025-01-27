51018cd967c6c66aa4386033160eb03755745d7d
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-27 12:39:32 -0700

    add "supportsDynamicLocale": true to sims that support it but didn't have it, https://github.com/phetsims/joist/issues/1005

--- js/VectorAdditionStrings.ts
+++ js/VectorAdditionStrings.ts
@@ -13,38 +13,24 @@ import vectorAddition from './vectorAddition.js';
 
 type StringsType = {
   'vector-addition': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
   'screen': {
-    'equations': string;
     'equationsStringProperty': LocalizedStringProperty;
-    'explore1D': string;
     'explore1DStringProperty': LocalizedStringProperty;
-    'explore2D': string;
     'explore2DStringProperty': LocalizedStringProperty;
-    'lab': string;
     'labStringProperty': LocalizedStringProperty;
   };
-  'sum': string;
   'sumStringProperty': LocalizedStringProperty;
-  'values': string;
   'valuesStringProperty': LocalizedStringProperty;
-  'components': string;
   'componentsStringProperty': LocalizedStringProperty;
-  'noVectorSelected': string;
   'noVectorSelectedStringProperty': LocalizedStringProperty;
-  'vectorValues': string;
   'vectorValuesStringProperty': LocalizedStringProperty;
   'symbol': {
-    'x': string;
     'xStringProperty': LocalizedStringProperty;
-    'y': string;
     'yStringProperty': LocalizedStringProperty;
   };
-  'baseVectors': string;
   'baseVectorsStringProperty': LocalizedStringProperty;
-  'equation': string;
   'equationStringProperty': LocalizedStringProperty;
 };
 
