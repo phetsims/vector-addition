7e866ee3a9aea18560dc990e932ac76b3d11054d
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:06 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/VectorAdditionQueryParameters.ts
+++ js/common/VectorAdditionQueryParameters.ts
@@ -8,6 +8,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import vectorAddition from '../vectorAddition.js';
 
 const VectorAdditionQueryParameters = QueryStringMachine.getAll( {
