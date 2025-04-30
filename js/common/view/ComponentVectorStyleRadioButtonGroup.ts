0665292be42ed39c2ea4a6524bbd1c0ed358f7f3
Chris Malley <cmalley@pixelzoom.com>
2025-04-30 14:51:28 -0600

    ComponentVectorStyleRadioButtonGroup extends RectangularRadioButtonGroup, https://github.com/phetsims/vector-addition/issues/299

--- js/common/view/ComponentVectorStyleRadioButtonGroup.ts
+++ js/common/view/ComponentVectorStyleRadioButtonGroup.ts
@@ -1,47 +1,41 @@
 // Copyright 2019-2025, University of Colorado Boulder
 
 /**
- * ComponentVectorStyleRadioButtonGroup is a group of radio buttons, arranged in a grid, for selecting component vector
- * style. It does not use RectangularRadioButtonGroup, because that class does not support a grid layout.
- * See https://github.com/phetsims/sun/issues/513 for context.
+ * ComponentVectorStyleRadioButtonGroup is a group of radio buttons, arranged in a grid, for selecting component vector style.
  *
  * @author Brandon Li
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import { combineOptions } from '../../../../phet-core/js/optionize.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
-import RectangularRadioButton, { RectangularRadioButtonOptions } from '../../../../sun/js/buttons/RectangularRadioButton.js';
 import vectorAddition from '../../vectorAddition.js';
 import { ComponentVectorStyle, ComponentVectorStyleValues } from '../model/ComponentVectorStyle.js';
-import VectorAdditionConstants from '../VectorAdditionConstants.js';
-import GridBox from './GridBox.js';
 import VectorAdditionIconFactory from './VectorAdditionIconFactory.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import StringUnionProperty from '../../../../axon/js/StringUnionProperty.js';
+import RectangularRadioButtonGroup, { RectangularRadioButtonGroupItem } from '../../../../sun/js/buttons/RectangularRadioButtonGroup.js';
 
-export default class ComponentVectorStyleRadioButtonGroup extends Node {
+export default class ComponentVectorStyleRadioButtonGroup extends RectangularRadioButtonGroup<ComponentVectorStyle> {
 
   public constructor( componentVectorStyleProperty: StringUnionProperty<ComponentVectorStyle>, tandem: Tandem ) {
 
-    // Create the radio buttons. Note that order of enum values determines order of buttons.
-    const buttons: RectangularRadioButton<ComponentVectorStyle>[] = [];
-    ComponentVectorStyleValues.forEach( componentVectorStyle => {
-      buttons.push( new RectangularRadioButton( componentVectorStyleProperty, componentVectorStyle,
-        combineOptions<RectangularRadioButtonOptions>( {}, VectorAdditionConstants.RADIO_BUTTON_GROUP_OPTIONS.radioButtonOptions, {
-          content: VectorAdditionIconFactory.createComponentStyleRadioButtonIcon( componentVectorStyle ),
-          tandem: tandem.createTandem( `${componentVectorStyle}RadioButton` )
-        } ) ) );
+    const items: RectangularRadioButtonGroupItem<ComponentVectorStyle>[] = ComponentVectorStyleValues.map( componentVectorStyle => {
+      return {
+        value: componentVectorStyle,
+        createNode: () => VectorAdditionIconFactory.createComponentStyleRadioButtonIcon( componentVectorStyle ),
+        tandemName: `${componentVectorStyle}RadioButton`
+      };
     } );
 
-    // Arrange the buttons in a grid
-    const gridBox = new GridBox( buttons, {
-      columns: 2
-    } );
-
-    super( {
-      children: [ gridBox ],
+    super( componentVectorStyleProperty, items, {
       isDisposable: false,
+
+      // These options implement a 2x2 grid.
+      orientation: 'horizontal',
+      preferredWidth: 120,
+      wrap: true,
+      spacing: 6,
+      lineSpacing: 6,
+      
       tandem: tandem
     } );
   }
