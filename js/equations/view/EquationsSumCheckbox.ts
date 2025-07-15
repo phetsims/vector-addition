d044c57c2a2eb684ea30a9dada0d7a918db9093c
pixelzoom <cmalley@pixelzoom.com>
2025-07-15 10:11:50 -0600

    revise core description for c and f checkboxes, https://github.com/phetsims/vector-addition/issues/312

--- js/equations/view/EquationsSumCheckbox.ts
+++ js/equations/view/EquationsSumCheckbox.ts
@@ -7,18 +7,25 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import Node from '../../../../scenery/js/nodes/Node.js';
 import vectorAddition from '../../vectorAddition.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import EquationsScene from '../model/EquationsScene.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
+import VectorAdditionSymbols from '../../common/VectorAdditionSymbols.js';
 import Property from '../../../../axon/js/Property.js';
+import ArrowOverSymbolNode from '../../common/view/ArrowOverSymbolNode.js';
+import AlignGroup from '../../../../scenery/js/layout/constraints/AlignGroup.js';
+import VectorAdditionIconFactory from '../../common/view/VectorAdditionIconFactory.js';
+import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
+import VectorAdditionConstants from '../../common/VectorAdditionConstants.js';
+import Checkbox from '../../../../sun/js/Checkbox.js';
 import DerivedStringProperty from '../../../../axon/js/DerivedStringProperty.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
 import VectorAdditionStrings from '../../VectorAdditionStrings.js';
-import SumCheckbox from '../../common/view/SumCheckbox.js';
-import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 
-export default class EquationsSumCheckbox extends SumCheckbox {
+export default class EquationsSumCheckbox extends Checkbox {
 
   public constructor( sumVisibleProperty: Property<boolean>,
                       sceneProperty: TReadOnlyProperty<EquationsScene>,
@@ -26,19 +33,55 @@ export default class EquationsSumCheckbox extends SumCheckbox {
                       polarScene: EquationsScene,
                       tandem: Tandem ) {
 
-    const symbolProperty = new DerivedStringProperty(
-      [ sceneProperty, cartesianScene.vectorSet.sumSymbolProperty, polarScene.vectorSet.sumSymbolProperty ],
-      scene => scene.vectorSet.sumSymbolProperty.value );
+    const cSymbolNode = new ArrowOverSymbolNode( VectorAdditionSymbols.cStringProperty, {
+      visibleProperty: new DerivedProperty( [ sceneProperty ], scene => scene === cartesianScene ),
+      maxWidth: 95 // determined empirically
+    } );
 
-    super( sumVisibleProperty, {
+    const fSymbolNode = new ArrowOverSymbolNode( VectorAdditionSymbols.fStringProperty, {
+      visibleProperty: new DerivedProperty( [ sceneProperty ], scene => scene === polarScene ),
+      maxWidth: 95 // determined empirically
+    } );
 
-      // Vector {{vectorSymbol}}
-      vectorIconFill: new DerivedProperty( [ sceneProperty ], scene => scene.vectorSet.vectorColorPalette.sumFill ),
-      vectorIconStroke: new DerivedProperty( [ sceneProperty ], scene => scene.vectorSet.vectorColorPalette.sumStroke ),
-      sumSymbolProperty: symbolProperty,
-      sumSymbolDescriptionProperty: symbolProperty,
+    const symbolProperty = new DerivedStringProperty( [ sceneProperty ], scene => scene.vectorSet.symbolProperties[ 2 ].value );
+
+    // To make both symbols have the same effective size, so that control panel layout doesn't shift around.
+    const alignGroup = new AlignGroup();
+
+    const symbolNode = new Node( {
+      children: [
+        alignGroup.createBox( cSymbolNode ),
+        alignGroup.createBox( fSymbolNode )
+      ]
+    } );
+
+    const vectorIcon = VectorAdditionIconFactory.createVectorIcon( 35, {
+      fill: new DerivedProperty( [ sceneProperty ], scene => scene.vectorSet.vectorColorPalette.sumFill ),
+      stroke: new DerivedProperty( [ sceneProperty ], scene => scene.vectorSet.vectorColorPalette.sumStroke )
+    } );
+
+    const hBox = new HBox( {
+      spacing: VectorAdditionConstants.CHECKBOX_ICON_SPACING,
+      children: [ symbolNode, vectorIcon ]
+    } );
+
+    // Wrap in a Node so that label and icon do not get separated when the checkbox layout is stretched.
+    const content = new Node( {
+      children: [ hBox ]
+    } );
+
+    super( sumVisibleProperty, content, {
       accessibleName: new PatternStringProperty( VectorAdditionStrings.a11y.equationsSumCheckbox.accessibleNameStringProperty, {
         vectorSymbol: symbolProperty
+      } ),
+      accessibleHelpText: new PatternStringProperty( VectorAdditionStrings.a11y.sumCheckbox.accessibleHelpTextStringProperty, {
+        sumVectorSymbol: symbolProperty
+      } ),
+      accessibleContextResponseChecked: new PatternStringProperty( VectorAdditionStrings.a11y.sumCheckbox.accessibleContextResponseCheckedStringProperty, {
+        sumVectorSymbol: symbolProperty
+      } ),
+      accessibleContextResponseUnchecked: new PatternStringProperty( VectorAdditionStrings.a11y.sumCheckbox.accessibleContextResponseUncheckedStringProperty, {
+        sumVectorSymbol: symbolProperty
       } ),
       tandem: tandem
     } );
