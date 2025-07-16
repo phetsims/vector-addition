59c22ee732a61f470a3f657018a0ae73449db98b
pixelzoom <cmalley@pixelzoom.com>
2025-07-16 16:28:01 -0600

    doc

--- js/common/VectorAdditionSymbols.ts
+++ js/common/VectorAdditionSymbols.ts
@@ -18,21 +18,26 @@ const VectorAdditionSymbols = {
 
   // Vector symbols are currently not localized. See https://github.com/phetsims/vector-addition/issues/10.
   // If they need to be localized in the future, dynamic layout was already handled as part of
-  // https://github.com/phetsims/vector-addition/issues/280. So localization should be a simple matter of
-  // adding these under "symbol" in vector-addition-strings_en.json.
+  // https://github.com/phetsims/vector-addition/issues/280. So localization should be a matter of
+  // adding these to vector-addition-strings_en.json, then using the LocalizedStringProperty instances
+  // in VectorAdditionStrings.ts.
+
+  // Vectors in the Explore 1D, Explore 2D, and Equations screens.
   aStringProperty: new StringProperty( 'a' ),
   bStringProperty: new StringProperty( 'b' ),
   cStringProperty: new StringProperty( 'c' ),
   dStringProperty: new StringProperty( 'd' ),
   eStringProperty: new StringProperty( 'e' ),
   fStringProperty: new StringProperty( 'f' ),
-  sStringProperty: new StringProperty( 's' ), // label for sum vectors
 
-  // Vectors in the Lab screen
+  // Vector sets in the Lab screen.
   vStringProperty: new StringProperty( 'v' ),
   uStringProperty: new StringProperty( 'u' ),
   pStringProperty: new StringProperty( 'p' ),
-  qStringProperty: new StringProperty( 'q' )
+  qStringProperty: new StringProperty( 'q' ),
+
+  // Sum vectors in the Explore 1D, Explore 2D, and Lab screens.
+  sStringProperty: new StringProperty( 's' )
 };
 
 vectorAddition.register( 'VectorAdditionSymbols', VectorAdditionSymbols );
