2e1fa160083d95a23050886ba5f373065716425e
phet-dev <phetadmin@gmail.com>
2025-10-14 04:48:48 -0600

    update copyright dates from daily grunt work

--- js/common/model/ExploreScene.ts
+++ js/common/model/ExploreScene.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2019-2025, University of Colorado Boulder
 
 /**
  * ExploreScene is the base class for scenes in the 'Explore 1D' and 'Explore 2D' screens.
