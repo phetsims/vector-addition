55ef83300600dcca02ebe4431261d089e128506f
pixelzoom <cmalley@pixelzoom.com>
2025-10-15 13:15:43 -0600

    Current Details for Equations screen, https://github.com/phetsims/vector-addition/issues/306

--- js/equations/view/EquationsScreenSummaryContent.ts
+++ js/equations/view/EquationsScreenSummaryContent.ts
@@ -12,19 +12,28 @@ import EquationsModel from '../model/EquationsModel.js';
 import VectorAdditionStrings from '../../VectorAdditionStrings.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
-import StringProperty from '../../../../axon/js/StringProperty.js';
+import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
+import EquationsScene from '../model/EquationsScene.js';
+import affirm from '../../../../perennial-alias/js/browser-and-node/affirm.js';
 
 export default class EquationsScreenSummaryContent extends ScreenSummaryContent {
 
   public constructor( model: EquationsModel ) {
 
+    // Control Area description
     const controlAreaStringProperty = new PatternStringProperty( VectorAdditionStrings.a11y.equationsScreen.screenSummary.controlAreaStringProperty, {
       symbol1: new DerivedProperty( [ model.sceneProperty ], scene => scene.vectorSet.allVectors[ 0 ].accessibleSymbolProperty.value ),
       symbol2: new DerivedProperty( [ model.sceneProperty ], scene => scene.vectorSet.allVectors[ 1 ].accessibleSymbolProperty.value ),
       symbol3: new DerivedProperty( [ model.sceneProperty ], scene => scene.vectorSet.resultantVector.accessibleSymbolProperty.value )
     } );
 
-    const currentDetailsStringProperty = new StringProperty( 'Currently, TODO' );
+    // Current Details description
+    const cartesianCurrentDetailsStringProperty = createCurrentDetailsStringProperty( model.cartesianScene );
+    const polarCurrentDetailsStringProperty = createCurrentDetailsStringProperty( model.polarScene );
+    const currentDetailsStringProperty = new DerivedProperty(
+      [ model.sceneProperty, cartesianCurrentDetailsStringProperty, polarCurrentDetailsStringProperty ],
+      ( scene, cartesianCurrentDetailsString, polarCurrentDetailsString ) =>
+        ( scene === model.cartesianScene ) ? cartesianCurrentDetailsString : polarCurrentDetailsString );
 
     super( {
       playAreaContent: VectorAdditionStrings.a11y.equationsScreen.screenSummary.playAreaStringProperty,
@@ -33,6 +42,40 @@ export default class EquationsScreenSummaryContent extends ScreenSummaryContent 
       interactionHintContent: VectorAdditionStrings.a11y.equationsScreen.screenSummary.interactionHintStringProperty
     } );
   }
+}
+
+/**
+ * Creates the current details for a scene.
+ */
+function createCurrentDetailsStringProperty( scene: EquationsScene ): TReadOnlyProperty<string> {
+
+  const vectorSet = scene.vectorSet;
+  affirm( vectorSet.allVectors.length === 2, 'Expected vectorSet to have 2 vectors.' );
+
+  // Options shared by all PatternStringProperty instances herein.
+  const patternStringPropertyOptions = {
+    coefficient1: vectorSet.allVectors[ 0 ].coefficientProperty,
+    symbol1: vectorSet.allVectors[ 0 ].accessibleSymbolProperty,
+    coefficient2: vectorSet.allVectors[ 1 ].coefficientProperty,
+    symbol2: vectorSet.allVectors[ 1 ].accessibleSymbolProperty,
+    symbol3: vectorSet.resultantVector.accessibleSymbolProperty
+  };
+
+  const additionStringProperty = new PatternStringProperty( VectorAdditionStrings.a11y.equationsScreen.screenSummary.currentDetailsAdditionStringProperty,
+    patternStringPropertyOptions );
+
+  const subtractionStringProperty = new PatternStringProperty( VectorAdditionStrings.a11y.equationsScreen.screenSummary.currentDetailsSubtractionStringProperty,
+    patternStringPropertyOptions );
+
+  const negationStringProperty = new PatternStringProperty( VectorAdditionStrings.a11y.equationsScreen.screenSummary.currentDetailsNegationStringProperty,
+    patternStringPropertyOptions );
+
+  return new DerivedProperty(
+    [ scene.equationTypeProperty, additionStringProperty, subtractionStringProperty, negationStringProperty ],
+    ( equationType, additionString, subtractionString, negationString ) =>
+      equationType === 'addition' ? additionString :
+      equationType === 'subtraction' ? subtractionString :
+      negationString );
 }
 
 vectorAddition.register( 'EquationsScreenSummaryContent', EquationsScreenSummaryContent );
