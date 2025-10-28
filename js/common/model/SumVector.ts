c307daf182ef7cc77c1bde9d3b4baf2494ec5ef6
pixelzoom <cmalley@pixelzoom.com>
2025-10-28 17:14:56 -0400

    simplify computation of sum vector, set correct initial value for sum xyComponentsProperty, https://github.com/phetsims/vector-addition/issues/258

--- js/common/model/SumVector.ts
+++ js/common/model/SumVector.ts
@@ -11,9 +11,9 @@
  *
  * @author Martin Veillette
  * @author Brandon Li
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import { ObservableArray } from '../../../../axon/js/createObservableArray.js';
 import Multilink from '../../../../axon/js/Multilink.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import vectorAddition from '../../vectorAddition.js';
@@ -25,7 +25,6 @@ import Graph from './Graph.js';
 import Property from '../../../../axon/js/Property.js';
 import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import { ComponentVectorStyle } from './ComponentVectorStyle.js';
-import isSettingPhetioStateProperty from '../../../../tandem/js/isSettingPhetioStateProperty.js';
 
 type SelfOptions = EmptySelfOptions;
 
@@ -42,59 +41,31 @@ export default class SumVector extends ResultantVector {
 
     const options = providedOptions;
 
-    // Initialize an arbitrary vector model. Its components and magnitude to be set later.
-    super( tailPosition, Vector2.ZERO, vectorSet, graph, selectedVectorProperty, componentVectorStyleProperty, options );
+    super( tailPosition, computeSum( vectorSet.allVectors ), vectorSet, graph, selectedVectorProperty, componentVectorStyleProperty, options );
 
-    // Observe changes to the vector array.
-    const vectorAddedListener = ( addedVector: Vector ) => {
-
-      // When the vector changes, update the sum calculation. Must be disposed.
-      const addedVectorMultilink = Multilink.multilink(
-        [ addedVector.xyComponentsProperty, addedVector.isOnGraphProperty ], () => {
-          if ( !isSettingPhetioStateProperty.value ) {
-            this.updateSum( vectorSet.activeVectors );
-          }
-        } );
-
-      // Clean up when the vector is removed.
-      const vectorRemovedListener = ( removedVector: Vector ) => {
-        if ( removedVector === addedVector ) {
-          this.updateSum( vectorSet.activeVectors );
-          addedVectorMultilink.dispose();
-          vectorSet.activeVectors.removeItemRemovedListener( vectorRemovedListener );
-        }
-      };
-      vectorSet.activeVectors.addItemRemovedListener( vectorRemovedListener );
-    };
-    vectorSet.activeVectors.addItemAddedListener( vectorAddedListener );
-    vectorSet.activeVectors.forEach( vector => vectorAddedListener( vector ) );
+    // When a vector is added or removed from the graph, or xy-components change, update the sum.
+    Multilink.multilinkAny( [
+        ...vectorSet.allVectors.map( vector => vector.isOnGraphProperty ),
+        ...vectorSet.allVectors.map( vector => vector.xyComponentsProperty )
+      ], () => {
+      this.xyComponentsProperty.value = computeSum( vectorSet.allVectors );
+    } );
   }
+}
 
-  /**
-   * WORKAROUND: xyComponentsProperty was initialized with a bogus value of Vector2.ZERO in the
-   * call to the superclass constructor above. So call updateSum after calling super.reset.
-   * See https://github.com/phetsims/vector-addition/issues/328.
-   */
-  public override reset(): void {
-    super.reset();
-    this.updateSum( this.vectorSet.activeVectors );
-  }
+/**
+ * Computes the sum of all vectors that are on the graph.
+ */
+function computeSum( vectors: Vector[] ): Vector2 {
 
-  /**
-   * Update the sum's xy-components. Calculated from all the vectors that are on the graph.
-   */
-  private updateSum( vectors: ObservableArray<Vector> ): void {
+  // Filter to get only the vectors that are on the graph.
+  const vectorsOnGraph = vectors.filter( vector => vector.isOnGraphProperty.value );
 
-    // Filter to get only the vectors that are on the graph.
-    const onGraphVectors = vectors.filter( vector => vector.isOnGraphProperty.value );
+  // Sum all vectors that are on the graph.
+  const sum = new Vector2( 0, 0 );
+  vectorsOnGraph.forEach( vector => sum.add( vector.xyComponents ) );
 
-    // Sum all vectors that are on the graph.
-    const sumVectorComponents = new Vector2( 0, 0 );
-    onGraphVectors.forEach( vector => sumVectorComponents.add( vector.xyComponents ) );
-
-    // Set the sum to the calculated sum.
-    this.xyComponentsProperty.value = sumVectorComponents;
-  }
+  return sum;
 }
 
 vectorAddition.register( 'SumVector', SumVector );
