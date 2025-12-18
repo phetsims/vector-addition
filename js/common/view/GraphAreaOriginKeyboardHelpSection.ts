89053e6e459c7f5f8a93961d5f5f37723c7536a9
Jesse <jesse.greenberg@colorado.edu>
2025-12-18 13:52:28 -0500

    Use SceneryPhetFluent in all repos using scenery-phet strings, see https://github.com/phetsims/scenery-phet/issues/958

--- js/common/view/GraphAreaOriginKeyboardHelpSection.ts
+++ js/common/view/GraphAreaOriginKeyboardHelpSection.ts
@@ -9,7 +9,7 @@
 import KeyboardHelpIconFactory from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpIconFactory.js';
 import KeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSection.js';
 import KeyboardHelpSectionRow from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSectionRow.js';
-import SceneryPhetStrings from '../../../../scenery-phet/js/SceneryPhetStrings.js';
+import SceneryPhetFluent from '../../../../scenery-phet/js/SceneryPhetFluent.js';
 import vectorAddition from '../../vectorAddition.js';
 import VectorAdditionStrings from '../../VectorAdditionStrings.js';
 
@@ -21,9 +21,9 @@ export default class GraphAreaOriginKeyboardHelpSection extends KeyboardHelpSect
     // Note that we are not using MoveDraggableItemsKeyboardHelpSection because a Shift modifier is not needed for the
     // origin, and MoveDraggableItemsKeyboardHelpSection has no way to omit the keyboard help for the Shift modifier.
     const moveRow = KeyboardHelpSectionRow.labelWithIcon(
-      SceneryPhetStrings.keyboardHelpDialog.moveStringProperty,
+      SceneryPhetFluent.keyboardHelpDialog.moveStringProperty,
       KeyboardHelpIconFactory.arrowOrWasdKeysRowIcon(), {
-        labelInnerContent: SceneryPhetStrings.a11y.keyboardHelpDialog.draggableItems.moveDescriptionStringProperty
+        labelInnerContent: SceneryPhetFluent.a11y.keyboardHelpDialog.draggableItems.moveDescriptionStringProperty
       } );
 
     // 'Graph Area Origin' title
