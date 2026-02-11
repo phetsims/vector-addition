b9c7da042d78cbbc9cb3ab862877e0c8e2fa5084
phet-dev <phetadmin@gmail.com>
2026-02-11 05:29:37 -0700

    update copyright dates from daily grunt work

--- js/common/view/GraphAreaOriginKeyboardHelpSection.ts
+++ js/common/view/GraphAreaOriginKeyboardHelpSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * GraphAreaOriginKeyboardHelpSection is the keyboard-help section that describes how to move the graph origin.
