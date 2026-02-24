f3ca86f1629552943268f6b2a2edbe789818324b
phet-dev <phetadmin@gmail.com>
2026-02-24 04:10:36 -0700

    update copyright dates from daily grunt work

--- js/common/view/VectorsKeyboardHelpSection.ts
+++ js/common/view/VectorsKeyboardHelpSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * VectorsKeyboardHelpSection is the keyboard-help section that describes how to interact with vectors.
